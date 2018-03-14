01c80e0042833ea282fc279dee2e5e04dc0bfaa4
bubbleboy14 <mario.balibrera@gmail.com>
2018-03-13 22:39:12 -0700

    Room: addCamera(), cut(), and support for cameras[] opt

--- ctzero/js/zero/core/Room.js
+++ ctzero/js/zero/core/Room.js
@@ -12,14 +12,30 @@ zero.core.Room = CT.Class({
 			this.opts.objects.push(obj);
 		this.objects.push(new zero.core.Thing(obj));
 	},
+	addCamera: function(cam) {
+		this.log("adding camera");
+		if (this.opts.cameras.indexOf(cam) == -1)
+			this.opts.cameras.push(cam);
+		this.cameras.push(cam);
+		this._cam = -1;
+	},
+	cut: function(index) {
+		if (typeof index != "number")
+			index = (this._cam + 1) % this.cameras.length;
+		this._cam = index;
+		zero.core.camera.move(this.cameras[this._cam]);
+	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(this.opts, opts, {
-			lights: [], // Lights
-			objects: [] // regular Things
+			lights: [],  // Lights
+			objects: [], // regular Things
+			cameras: []
 		});
 		this.lights = [];
 		this.objects = [];
+		this.cameras = [];
 		opts.lights.forEach(this.addLight);
 		opts.objects.forEach(this.addObject);
+		opts.cameras.forEach(this.addCamera);
 	}
 }, zero.core.Thing);
