fc2abaf6b2d6efb2e144b90b45fb4a65497b374a
bubbleboy14 <mario.balibrera@gmail.com>
2018-06-04 23:29:26 -0700

    click module!

---
+++ ctzero/js/zero/core/click.js
@@ -0,0 +1,35 @@
+zero.core.click = {
+    targets: [],
+    init: function() {
+        // adapted from: https://stemkoski.github.io/Three.js/Mouse-Click.html
+        if (zero.core.click._ready) return;
+        zero.core.click._ready = true;
+        var mouse = {}, projector = new THREE.Projector(),
+            cam = zero.core.camera.get(), vector, ray, intersects, i;
+        document.getElementsByTagName("canvas")[0].parentNode.addEventListener('mousedown', function(e) {
+            mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+            vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
+            projector.unprojectVector(vector, cam);
+            ray = new THREE.Raycaster(cam.position, vector.sub(cam.position).normalize());
+            intersects = ray.intersectObjects(zero.core.click.targets, true);
+
+            for (i = 0; i < intersects.length; i++) {
+                var obj = intersects[i].object;
+                while (obj) {
+                    if (obj.__click)
+                        return obj.__click();
+                    obj = obj.parent;
+                }
+            }
+        });
+    },
+    register: function(thing, cb) {
+        zero.core.click.init();
+        var thring = thing.bone || thing.thring;
+        thring.__click = cb;
+        thring.__name = thing.name;
+        zero.core.click.targets.push(thring);
+    }
+};
