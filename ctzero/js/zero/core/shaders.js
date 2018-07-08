f09095ee53f0e4c5bf60bd526db467503fd2524f
bubbleboy14 <mario.balibrera@gmail.com>
2018-07-08 03:59:30 -0700

    shaders: uniforms() adds morphStack uniforms; attributes(), which adds morphStack attributes; vertex() adds uniform imports (for aspects) and needs morph logic (todo)

--- ctzero/js/zero/core/shaders.js
+++ ctzero/js/zero/core/shaders.js
@@ -7,14 +7,45 @@ zero.core.shaders = {
 				value: map
 			};
 		}
+		if (thang.morphStack) {
+			for (var m in thang.morphStack) {
+				uz[m] = {
+					type: 'f',
+					value: 0.0
+				}
+			}
+		}
 		return uz;
+	},
+	attributes: function(thang) {
+		var az = {};
+		if (thang.morphStack) {
+			for (var m in thang.morphStack) {
+				az[m] = {
+					type: 'f',
+					value: thang.morphStack[m]
+				}
+			}
+		}
+		return az;
 	},
 	fragment: function(thang) {
 		return CT.net.get("/js/shaders/basic.frag");
 	},
 	vertex: function(thang) {
 		var vert = CT.net.get("/js/shaders/basic.vert");
+		if (thang.morphStack) {
+			vert = vert.replace("// MORPH IMPORTS HERE",
+				Object.keys(thang.morphStack).map(function(m) {
+					return "uniform float " + m;
+				}).join(";\n")).replace("// MORPH LOGIC HERE",
+				Object.keys(thang.morphStack).map(function(m) {
+					var morph = thang.morphStack[m];
 
+					return ""; // TODO: mod pos!!
+
+				}).join(";\n"));
+		}
 		return vert;
 	}
 };
