5967fc852cac146e5731c944da194eeaff6b840e
bubbleboy14 <mario.balibrera@gmail.com>
2018-11-10 19:41:59 -0800

    /_zero request handler, "json" action: special-case ctuser queries (no json() func) -- return object w/ people[] and rooms[] (json()) arrays

--- ctzero/_zero.py
+++ ctzero/_zero.py
@@ -1,11 +1,17 @@
 from cantools.web import log, respond, succeed, cgi_get, read_file
 from ctzero.speech import chat, say, rec
-from model import db
+from model import db, Person, Room
 
 def response():
     action = cgi_get("action", choices=["say", "rec", "chat", "json"])
     if action == "json": # better name?
-        succeed(db.get(cgi_get("key")).json())
+        ent = db.get(cgi_get("key"))
+        if ent.polytype == "ctuser":
+            succeed({
+                "people": [p.json() for p in Person.query(Person.owner == ent.key).fetch()],
+                "rooms": [r.json() for r in Room.query(Room.owner == ent.key).fetch()]
+            })
+        succeed(ent.json())
     elif action == "chat":
         succeed(chat(cgi_get("question")))
     language = cgi_get("language")
