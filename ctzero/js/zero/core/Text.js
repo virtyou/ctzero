32425b672d49bd5a1feaddde8b60bfdccc2f352c
bubbleboy14 <mario.balibrera@gmail.com>
2018-11-27 22:30:09 -0800

    zero.core.Text initial working version - todo: improve default positioning logic; probs speed up

---
+++ ctzero/js/zero/core/Text.js
@@ -0,0 +1,29 @@
+zero.core.Text = CT.Class({
+	CLASSNAME: "zero.core.Text",
+	init: function(opts) {
+		var noPos = !("position" in opts),
+			build = this.build;
+		var opts = this.opts = CT.merge(opts, {
+			size: 4,
+			height: 4,
+			deferBuild: true,
+			font: "https://threejs.org/examples/fonts/helvetiker_bold.typeface.json"
+		}, this.opts);
+		(new THREE.FontLoader()).load(opts.font, function(font) {
+			var geo = opts.geometry = new THREE.TextGeometry(opts.text, {
+				size: opts.size,
+				height: opts.height,
+				font: font
+			});
+			if (noPos) {
+				geo.computeBoundingBox();
+				opts.position = [
+					-0.5 * (geo.boundingBox.max.x - geo.boundingBox.min.x),
+					30,
+					0
+				];
+			}
+			build();
+		});
+	}
+}, zero.core.Thing);
