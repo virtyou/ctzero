ccb68467035b4ebd4e4d281eb090053d73c833c4
bubbleboy14 <mario.balibrera@gmail.com>
2018-11-30 17:30:56 -0800

    Menu: decent framing and autopositioning

--- ctzero/js/zero/core/Menu.js
+++ ctzero/js/zero/core/Menu.js
@@ -2,20 +2,34 @@ zero.core.Menu = CT.Class({
 	CLASSNAME: "zero.core.Menu",
 	init: function(opts) {
 		opts = this.opts = CT.merge(opts, this.opts, {
-			width: 15
+			width: 15,
+			depth: 20,
+			height: 20,
+			color: 0x00ff00
 		});
+		if (!(opts.cubeGeometry || opts.geometry)) {
+			opts.cubeGeometry = [opts.width * opts.items.length, opts.height, opts.depth];
+			opts.material = CT.merge(opts.material, {
+				transparent: true,
+				color: opts.color,
+				opacity: 0.2
+			});
+		}
 		// TODO: figure out dimensions, position, orientation based on camera
+		var offset = opts.width * opts.items.length / 2,
+			y = -opts.height / 2;
 		opts.parts = opts.items.map(function(part, i) {
+			var x = i * opts.width - offset;
 			return {
 				parts: [
 					CT.merge({
-						position: [i * opts.width, 20, 0],
+						position: [x, 10 + y, 0],
 						scale: [0.5, 0.5, 0.5]
 					}, part),
 					{
 						thing: "Text",
 						text: part.name,
-						position: [i * opts.width, 0, 0]
+						position: [x, y, 0]
 					}
 				]
 			};
