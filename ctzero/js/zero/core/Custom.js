3adaa5470d318c11f661334d94b4b011ea6a2312
bubbleboy14 <mario.balibrera@gmail.com>
2018-12-02 16:13:54 -0800

    zero.core.Custom, for encapsulating arbitrarily-defined content

---
+++ ctzero/js/zero/core/Custom.js
@@ -0,0 +1,19 @@
+zero.core.Custom = CT.Class({
+	CLASSNAME: "zero.core.Custom",
+	build: function() {
+		var thiz = this;
+		if (typeof this.opts.custom == "string") // from server
+			this.opts.custom = eval(this.opts.custom);
+		this.group = new THREE.Object3D();
+		// custom() must:
+		// - call iterator() post-init
+		// - return object with tick() (bonus points: name, thrings[])
+		this.tick = this.opts.custom({
+			scene: this.group,
+			iterator: function() {
+				thiz.opts.scene.add(thiz.group);
+				thiz._.built();
+			}
+		}).tick; // thrings[]?
+	}
+}, zero.core.Thing);
