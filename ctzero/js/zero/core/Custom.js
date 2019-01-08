c10d26cd47bbc9eef033dcec7dc4a8b02db9ac74
bubbleboy14 <mario.balibrera@gmail.com>
2019-01-07 21:26:22 -0800

    Custom: setName(), mostly extracted from now much smaller build()

--- ctzero/js/zero/core/Custom.js
+++ ctzero/js/zero/core/Custom.js
@@ -1,21 +1,26 @@
 zero.core.Custom = CT.Class({
 	CLASSNAME: "zero.core.Custom",
 	build: function() {
-		var thiz = this;
-		this.group = new THREE.Object3D();
 		this.isCustom = true;
 		this.place();
+	},
+	setName: function(opts) {
+		var thiz = this;
+		this.group = new THREE.Object3D();
 		// custom() must:
 		// - call iterator() post-init
 		// - return object with tick() (bonus points: name, thrings[])
 		if (typeof this.opts.custom == "string") // from server
 			this.opts.custom = eval(this.opts.custom);
-		this.tick = this.opts.custom({
+		this.custom_rep = this.opts.custom({
 			scene: this.group,
 			iterator: function() {
 				thiz.opts.scene.add(thiz.group);
 				thiz._.built();
 			}
-		}).tick; // thrings[]?
+		}); // thrings[]?
+		this.tick = this.custom_rep.tick;
+		this.name = opts.name = this.custom_rep.name || opts.name;
+		this.path = opts.path ? (opts.path + "." + opts.name) : opts.name;
 	}
 }, zero.core.Thing);
