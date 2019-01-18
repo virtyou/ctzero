e942fb87b28f58977eb0d933a1a7582c5575df14
bubbleboy14 <mario.balibrera@gmail.com>
2019-01-17 16:50:56 -0800

    Energy.init(): fixed opts merge

--- ctzero/js/zero/core/Energy.js
+++ ctzero/js/zero/core/Energy.js
@@ -1,7 +1,7 @@
 zero.core.Energy = CT.Class({
 	CLASSNAME: "zero.core.Energy",
 	init: function(opts) {
-		this.opts = CT.merge(opts, {
+		this.opts = opts = CT.merge(opts, {
 			k: 1,
 			damp: 1,
 			targetMult: 1,
