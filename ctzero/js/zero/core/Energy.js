7654eec29ad91a24fceda81c6cfc331df10438ec
bubbleboy14 <mario.balibrera@gmail.com>
2019-01-17 16:44:43 -0800

    Energy: added targetMult, reschedMult, and default opts{} merge

--- ctzero/js/zero/core/Energy.js
+++ ctzero/js/zero/core/Energy.js
@@ -1,8 +1,15 @@
 zero.core.Energy = CT.Class({
 	CLASSNAME: "zero.core.Energy",
 	init: function(opts) {
-		this.opts = opts;
+		this.opts = CT.merge(opts, {
+			k: 1,
+			damp: 1,
+			targetMult: 1,
+			reschedMult: 1
+		});
 		this.k = opts.k;
 		this.damp = opts.damp;
+		this.targetMult = opts.targetMult;
+		this.reschedMult = opts.reschedMult;
 	}
 });
