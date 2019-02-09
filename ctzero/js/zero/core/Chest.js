130bb5aeeca8ab62b74a4d40f5f1b253609c6937
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-08 16:52:10 -0800

    Chest: _bmap{left,right}; setBone() wires up _bmap-defined parts

--- ctzero/js/zero/core/Chest.js
+++ ctzero/js/zero/core/Chest.js
@@ -1,8 +1,18 @@
 zero.core.Chest = CT.Class({
 	CLASSNAME: "zero.core.Chest",
+	_bmap: {
+		left: 3,
+		right: 25
+	},
 	setBone: function() {
 		this.bones = this.opts.bones.slice();
 		this.bone = this.bones.shift();
-		// TODO: wire up arms
+
+		// TODO: wire up arms! -- the following doesn't work :-\
+		var k, b, bones = this.thring.skeleton.bones;
+		for (k in this._bmap) {
+			b = this[k] = bones[this._bmap[k]];
+			this.opts.scene.add(b);
+		}
 	}
 }, zero.core.Thing);
