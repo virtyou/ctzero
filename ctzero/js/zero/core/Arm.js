4f1f1c5a22b9af43b43a888be68044e47f829476
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-08 22:25:39 -0800

    Arm.setJoints(): create Hand

--- ctzero/js/zero/core/Arm.js
+++ ctzero/js/zero/core/Arm.js
@@ -3,8 +3,12 @@ zero.core.Arm = CT.Class({
 	setJoints: function() {
 		var part, oz = this.opts,
 			bones = oz.bones, bmap = oz.bonemap;
-		for (part in bmap)
-			this[part] = bones[bmap[part]];
+		for (part in bmap.arm)
+			this[part] = bones[bmap.arm[part]];
+		this.hand = new zero.core.Hand({
+			bones: bones,
+			bonemap: bmap.hand
+		});
 	},
 	init: function(opts) {
 		this.opts = CT.merge(opts, {
