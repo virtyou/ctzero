5b6e4a1e5adb9c5686a77ca2d3ee8ab0d0d79755
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-08 17:06:43 -0800

    Chest: _roots[], _bmap{left|right:{shoulder,elbo}}; setBone() attaches roots to scene and parts (shoulder, elbo) to each side (left, right); rotations on these bones do not yet seem to work!

--- ctzero/js/zero/core/Chest.js
+++ ctzero/js/zero/core/Chest.js
@@ -1,18 +1,31 @@
 zero.core.Chest = CT.Class({
 	CLASSNAME: "zero.core.Chest",
+	_roots: [3, 25],
 	_bmap: {
-		left: 3,
-		right: 25
+		left: {
+			shoulder: 4,
+			elbo: 5
+		},
+		right: {
+			shoulder: 26,
+			elbo: 27
+		}
 	},
 	setBone: function() {
 		this.bones = this.opts.bones.slice();
 		this.bone = this.bones.shift();
 
-		// TODO: wire up arms! -- the following doesn't work :-\
-		var k, b, bones = this.thring.skeleton.bones;
-		for (k in this._bmap) {
-			b = this[k] = bones[this._bmap[k]];
-			this.opts.scene.add(b);
+		// TODO: wire up arms!
+		// -- the following doesn't quite work w/ rotations... :-\
+		var side, part, b, scene = this.opts.scene,
+			bones = this.thring.skeleton.bones;
+		this._roots.forEach(function(root) {
+			scene.add(bones[root]);
+		});
+		for (side in this._bmap) {
+			this[side] = {};
+			for (part in this._bmap[side])
+				this[side][part] = bones[this._bmap[side][part]];
 		}
 	}
 }, zero.core.Thing);
