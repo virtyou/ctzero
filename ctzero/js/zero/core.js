ab936c945c89097e886195ff829fdd82d6878d2b
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-08 16:23:50 -0800

    added Chest to core imports

--- ctzero/js/zero/core.js
+++ ctzero/js/zero/core.js
@@ -23,6 +23,7 @@ CT.require("zero.core.Light");
 CT.require("zero.core.Pool");
 CT.require("zero.core.Room");
 CT.require("zero.core.Head");
+CT.require("zero.core.Chest");
 CT.require("zero.core.Body");
 CT.require("zero.core.Custom");
 
