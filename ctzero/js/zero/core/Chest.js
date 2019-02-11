df40426913b9885cb370ae1fdc4c2b2e14f88e80
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-10 21:04:49 -0800

    Chest.setArms(): get bonemap via CT.net.get()

--- ctzero/js/zero/core/Chest.js
+++ ctzero/js/zero/core/Chest.js
@@ -1,36 +1,8 @@
 zero.core.Chest = CT.Class({
 	CLASSNAME: "zero.core.Chest",
-	_bmap: {
-		left: {
-			arm: {
-				shoulder: 4,
-				elbo: 5
-			},
-			hand: {
-				thumb: [18, 19, 20],
-				pointer: [14, 15, 16, 17],
-				middle: [10, 11, 12, 13],
-				ring: [21, 22, 23, 24],
-				pinky: [6, 7, 8, 9]
-			}
-		},
-		right: {
-			arm: {
-				shoulder: 26,
-				elbo: 27
-			},
-			hand: {
-				thumb: [40, 41, 42],
-				pointer: [36, 37, 38, 39],
-				middle: [32, 33, 34, 35],
-				ring: [43, 44, 45, 46],
-				pinky: [28, 29, 30, 31]
-			}
-		}
-	},
 	tick: function() {
-		this.arms.left.tick();
-		this.arms.right.tick();
+		for (var arm in this.arms)
+			this.arms[arm].tick();
 	},
 	move: function(opts) {
 		for (var side in opts)
@@ -40,15 +12,19 @@ zero.core.Chest = CT.Class({
 		return this.body && this.body.energy();
 	},
 	setArms: function() {
-		var side, bones = this.thring.skeleton.bones;
+		var thiz = this, bones = this.thring.skeleton.bones;
 		this.arms = {};
-		for (side in this._bmap) {
-			this.arms[side] = new zero.core.Arm({
-				parent: this,
+
+		// faster way to get bmap, such as from three somehow?
+		this._bmap = CT.net.get(this.opts.stripset, null, true).bonemap;
+
+		["left", "right"].forEach(function(side) {
+			thiz.arms[side] = new zero.core.Arm({
+				parent: thiz,
 				bones: bones,
-				bonemap: this._bmap[side]
+				bonemap: thiz._bmap[side]
 			});
-		}
+		});
 	},
 	setBone: function() {
 		this.bones = this.opts.bones.slice();
@@ -56,7 +32,7 @@ zero.core.Chest = CT.Class({
 
 		var bones = this.thring.skeleton.bones;
 		this.opts.scene.add(bones[0]);
-		bones[0].position.y = 0; // HACK! fix this...
+		bones[0].position.y = -14; // HACK! fix this...
 
 		this.setArms();
 	}
