a0f718955dedeaee73fc471f79be21edf16d527e
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-14 21:48:55 -0800

    Hand, Arm: extracted most stuff and added Skeleton inheritance

--- ctzero/js/zero/core/Arm.js
+++ ctzero/js/zero/core/Arm.js
@@ -1,75 +1,20 @@
 zero.core.Arm = CT.Class({
 	CLASSNAME: "zero.core.Arm",
-	setJoints: function() {
-		var part, oz = this.opts, thiz = this,
-			bones = oz.bones, bmap = oz.bonemap,
-			sname, aspringz, dimz, drule,
-			springs = this.springs = {},
-			aspects = this.aspects = {};
-		for (part in bmap.arm) {
-			this[part] = bones[bmap.arm[part]];
-			dimz = zero.base.aspects.arm[part];
-			Object.keys(dimz).forEach(function(dim) {
-				sname = part + "_" + dim;
-				springs[sname] = zero.core.springController.add({
-					k: 20,
-					damp: 10
-				}, sname, thiz);
-				aspringz = {};
-				aspringz[sname] = 1;
-				if (oz.side == "left" && sname == "shoulder_z") {
-					drule = {
-						max: -dimz[dim].min,
-						min: -dimz[dim].max
-					};
-				} else
-					drule = dimz[dim];
-				aspects[sname] = zero.core.aspectController.add(CT.merge({
-					springs: aspringz
-				}, drule), sname, thiz);
-			});
-		}
+	bmap: function() {
+		return this.opts.bonemap.arm;
+	},
+	move: function(opts) {
+		this.setSprings(opts.arm);
+		this.hand.move(opts.hand);
+	},
+	build: function() {
+		var oz = this.opts, bones = oz.bones, bmap = oz.bonemap;
+		this.setJoints();
 		this.hand = new zero.core.Hand({
 			parent: this,
 			bones: bones,
 			bonemap: bmap.hand
 		});
-	},
-	rotation: function(part) {
-		var r = {}, asp, asps = this.aspects;
-		["x", "y", "z"].forEach(function(dim) {
-			asp = asps[part + "_" + dim];
-			if (asp)
-				r[dim] = asp.value;
-		});
-		return r;
-	},
-	tick: function() {
-		var thiz = this;
-		zero.core.Arm.parts.forEach(function(part) {
-			zero.core.util.update(thiz.rotation(part), thiz[part].rotation);
-		});
-		this.hand.tick();
-	},
-	move: function(opts) {
-		var part, springs = this.springs;
-		for (part in opts.arm) {
-			zero.core.util.coords(opts.arm[part], function(dim, val) {
-				springs[part + "_" + dim].target = val;
-			});
-		}
-		this.hand.move(opts.hand);
-	},
-	energy: function() {
-		return this.parent.energy();
-	},
-	init: function(opts) {
-		this.opts = CT.merge(opts, {
-			bones: [],
-			bonemap: {}
-		});
-		this.parent = opts.parent;
-		this.setJoints();
 	}
-});
+}, zero.core.Skeleton);
 zero.core.Arm.parts = ["shoulder", "elbow", "wrist"];
--- ctzero/js/zero/core/Hand.js
+++ ctzero/js/zero/core/Hand.js
@@ -1,68 +1,30 @@
 zero.core.Hand = CT.Class({
 	CLASSNAME: "zero.core.Hand",
-	setJoint: function(digit, dim) {
-		var aspringz, sname = digit + "_" + dim;
-		this.springs[sname] = zero.core.springController.add({
-			k: 20,
-			damp: 10
-		}, sname, this);
-		aspringz = {};
-		aspringz[sname] = 1;
-		this.aspects[sname] = zero.core.aspectController.add(CT.merge({
-			springs: aspringz
-		}, zero.base.aspects.hand[digit][dim]), sname, this);
-	},
 	majMin: function(digit) {
 		if (digit == "thumb")
 			return ["x", "y"];
 		else
 			return ["z", "x"];
 	},
-	setJoints: function() {
-		var digit, oz = this.opts, setJoint = this.setJoint,
-			bones = oz.bones, bmap = oz.bonemap;
-		this.springs = {};
-		this.aspects = {};
-		for (digit in bmap) {
-			this.majMin(digit).forEach(function(dim) {
-				setJoint(digit, dim);
-			});
-			this[digit] = bmap[digit].map(function(knuckle, i) {
-				return bones[knuckle];
-			});
-		}
-	},
-	tick: function() {
-		var major, minor, majMin = this.majMin,
-			thiz = this, knuckle, zval;
-		zero.core.Hand.parts.forEach(function(digit) {
-			[major, minor] = majMin(digit);
-			thiz[digit][0].rotation.x = thiz.aspects[digit + "_" + minor].value;
-			zval = thiz.aspects[digit + "_" + major].value;
-			for (knuckle = 0; knuckle < thiz[digit].length; knuckle++)
-				thiz[digit][knuckle].rotation.z = zval;
+	setPart: function(bmap, digit) {
+		var bones = this.opts.bones,
+			setJoint = this.setJoint;
+		this.majMin(digit).forEach(function(dim) {
+			setJoint(digit, dim);
+		});
+		this[digit] = bmap[digit].map(function(knuckle, i) {
+			return bones[knuckle];
 		});
 	},
-	move: function(opts) {
-		var part, springs = this.springs;
-		for (part in opts) {
-			zero.core.util.coords(opts[part], function(dim, val) {
-				springs[part + "_" + dim].target = val;
-			});
-		}
-	},
-	energy: function() {
-		return this.parent.energy();
-	},
-	init: function(opts) {
-		this.opts = CT.merge(opts, {
-			bones: [],
-			bonemap: {}
-		});
-		this.parent = opts.parent;
-		this.setJoints();
+	tickPart: function(digit) {
+		var major, minor, knuckle, zval;
+		[major, minor] = this.majMin(digit);
+		this[digit][0].rotation.x = this.aspects[digit + "_" + minor].value;
+		zval = this.aspects[digit + "_" + major].value;
+		for (knuckle = 0; knuckle < this[digit].length; knuckle++)
+			this[digit][knuckle].rotation.z = zval;
 	}
-});
+}, zero.core.Skeleton);
 
 zero.core.Hand.parts = ["thumb", "pointer", "middle", "ring", "pinkie"];
 zero.core.Hand.parts.forEach(function(digit) {
