a6a0e12f7085c60e139f4cdd85c13f1329bc1124
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-14 21:47:26 -0800

    core imports: added Skeleton; reorder

--- ctzero/js/zero/core.js
+++ ctzero/js/zero/core.js
@@ -23,10 +23,12 @@ CT.require("zero.core.Light");
 CT.require("zero.core.Pool");
 CT.require("zero.core.Room");
 
-CT.require("zero.core.Head");
-CT.require("zero.core.Chest");
-CT.require("zero.core.Arm");
+CT.require("zero.core.Skeleton");
 CT.require("zero.core.Hand");
+CT.require("zero.core.Arm");
+
+CT.require("zero.core.Chest");
+CT.require("zero.core.Head");
 CT.require("zero.core.Body");
 CT.require("zero.core.Custom");
 
