915179581fd2fac987f28bf72c59d8145a574e7a
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-14 18:20:24 -0800

    Hand: majMin(); custom thumb constraints

--- ctzero/js/zero/core/Hand.js
+++ ctzero/js/zero/core/Hand.js
@@ -12,33 +12,42 @@ zero.core.Hand = CT.Class({
 			springs: aspringz
 		}, zero.base.aspects.hand[digit][dim]), sname, this);
 	},
+	majMin: function(digit) {
+		if (digit == "thumb")
+			return ["x", "y"];
+		else
+			return ["z", "x"];
+	},
 	setJoints: function() {
 		var digit, oz = this.opts, setJoint = this.setJoint,
 			bones = oz.bones, bmap = oz.bonemap;
 		this.springs = {};
 		this.aspects = {};
 		for (digit in bmap) {
-			setJoint(digit, "x");
-			setJoint(digit, "z");
+			this.majMin(digit).forEach(function(dim) {
+				setJoint(digit, dim);
+			});
 			this[digit] = bmap[digit].map(function(knuckle, i) {
 				return bones[knuckle];
 			});
 		}
 	},
 	tick: function() {
-		var knuckle, thiz = this, zval;
+		var major, minor, majMin = this.majMin,
+			thiz = this, knuckle, zval;
 		zero.core.Hand.parts.forEach(function(digit) {
-			thiz[digit][0].rotation.x = thiz.aspects[digit + "_x"].value;
-			zval = thiz.aspects[digit + "_z"].value;
+			[major, minor] = majMin(digit);
+			thiz[digit][0].rotation.x = thiz.aspects[digit + "_" + minor].value;
+			zval = thiz.aspects[digit + "_" + major].value;
 			for (knuckle = 0; knuckle < thiz[digit].length; knuckle++)
 				thiz[digit][knuckle].rotation.z = zval;
 		});
 	},
 	move: function(opts) {
-		var part, val, springs = this.springs;
+		var part, springs = this.springs;
 		for (part in opts) {
-			["x", "z"].forEach(function(dim) {
-				springs[part + "_" + dim].target = opts[part][dim];
+			zero.core.util.coords(opts[part], function(dim, val) {
+				springs[part + "_" + dim].target = val;
 			});
 		}
 	},
@@ -57,7 +66,16 @@ zero.core.Hand = CT.Class({
 
 zero.core.Hand.parts = ["thumb", "pointer", "middle", "ring", "pinkie"];
 zero.core.Hand.parts.forEach(function(digit) {
-    zero.base.aspects.hand[digit] = {
+    zero.base.aspects.hand[digit] = (digit == "thumb") ? {
+        x: {
+            max: 0.3,
+            min: -0.2
+        },
+        y: {
+            max: 0.2,
+            min: -0.6
+        }
+    } : {
         z: {
             max: 0,
             min: -1.5
