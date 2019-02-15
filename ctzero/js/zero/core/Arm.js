fb398f2aeca4ef73d859825d92455a8c7573f680
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-14 21:59:19 -0800

    Arm: tick() w/ hand.tick(); build() passes opts.side to Hand

--- ctzero/js/zero/core/Arm.js
+++ ctzero/js/zero/core/Arm.js
@@ -7,13 +7,18 @@ zero.core.Arm = CT.Class({
 		this.setSprings(opts.arm);
 		this.hand.move(opts.hand);
 	},
+	tick: function() {
+		zero.core[this.variety].parts.forEach(this.tickPart);
+		this.hand.tick();
+	},
 	build: function() {
 		var oz = this.opts, bones = oz.bones, bmap = oz.bonemap;
 		this.setJoints();
 		this.hand = new zero.core.Hand({
 			parent: this,
 			bones: bones,
-			bonemap: bmap.hand
+			bonemap: bmap.hand,
+			side: this.opts.side
 		});
 	}
 }, zero.core.Skeleton);
