00021ddb243a46aa1e07ba3a691bd84004a862cb
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-15 17:40:57 -0800

    Chest.setBone(): adjusted bone[0] y pos for full body

--- ctzero/js/zero/core/Chest.js
+++ ctzero/js/zero/core/Chest.js
@@ -35,7 +35,7 @@ zero.core.Chest = CT.Class({
 
 		var bones = this.thring.skeleton.bones;
 		this.opts.scene.add(bones[0]);
-		bones[0].position.y = -14.3; // HACK! fix this...
+		bones[0].position.y = -3.7; // HACK! fix this...
 
 		this.setArms();
 	}
