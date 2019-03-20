5646cb54444d01032bd9f3c33268ddd03889b5fd
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-20 15:00:08 -0700

    Room.setBounds(): check for / use existing bounds box

--- ctzero/js/zero/core/Room.js
+++ ctzero/js/zero/core/Room.js
@@ -10,7 +10,7 @@ zero.core.Room = CT.Class({
 		});
 	},
 	setBounds: function() {
-		this.bounds = new THREE.Box3();
+		this.bounds = this.bounds || new THREE.Box3();
 		this.bounds.setFromObject(this.thring);
 		(Object.values(zero.core.current.people)).forEach(function(person) {
 			person.body.setBounds();
