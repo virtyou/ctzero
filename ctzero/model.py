0f9f0c6c036f1095b30128b3ad9a0cc63fc62d2c
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-23 16:08:54 -0700

    model: whitespace fixes

--- ctzero/model.py
+++ ctzero/model.py
@@ -2,158 +2,158 @@ from cantools import db
 from ctuser.model import CTUser
 
 class Asset(db.TimeStampedBase):
-	owner = db.ForeignKey(kind=CTUser)
-	variety = db.String(choices=["texture", "stripset"])
-	name = db.String()
-	identifier = db.String()
-	item = db.Binary(unique=True)
+    owner = db.ForeignKey(kind=CTUser)
+    variety = db.String(choices=["texture", "stripset"])
+    name = db.String()
+    identifier = db.String()
+    item = db.Binary(unique=True)
 
-	def json(self):
-		return self.data()
+    def json(self):
+        return self.data()
 
 class Thing(db.TimeStampedBase):
-	owner = db.ForeignKey(kind=CTUser)
-	texture = db.ForeignKey(kind=Asset)
-	stripset = db.ForeignKey(kind=Asset)
-	morphStack = db.String() # should be Asset, but Thing.js gets complicated...
-	kind = db.String() # furnishing, headgear, head, eye, arm, leg, etc
-	name = db.String()
-	custom = db.Text()
-	material = db.JSON()
-	morphs = db.JSON()
-	opts = db.JSON() # base opts
+    owner = db.ForeignKey(kind=CTUser)
+    texture = db.ForeignKey(kind=Asset)
+    stripset = db.ForeignKey(kind=Asset)
+    morphStack = db.String() # should be Asset, but Thing.js gets complicated...
+    kind = db.String() # furnishing, headgear, head, eye, arm, leg, etc
+    name = db.String()
+    custom = db.Text()
+    material = db.JSON()
+    morphs = db.JSON()
+    opts = db.JSON() # base opts
 
-	def json(self):
-		d = {
-			"key": self.key.urlsafe(),
-			"name": self.name,
-			"kind": self.kind,
-			"custom": self.custom,
-			"material": self.material,
-			"morphs": self.morphs or {},
-			"morphStack": self.morphStack,
-			"texture": self.texture and self.texture.get().item.urlsafe() or None,
-			"stripset": self.stripset and self.stripset.get().item.urlsafe() or None
-		}
-		self.opts and d.update(self.opts)
-		return d
+    def json(self):
+        d = {
+            "key": self.key.urlsafe(),
+            "name": self.name,
+            "kind": self.kind,
+            "custom": self.custom,
+            "material": self.material,
+            "morphs": self.morphs or {},
+            "morphStack": self.morphStack,
+            "texture": self.texture and self.texture.get().item.urlsafe() or None,
+            "stripset": self.stripset and self.stripset.get().item.urlsafe() or None
+        }
+        self.opts and d.update(self.opts)
+        return d
 
 class Part(db.TimeStampedBase):
-	parent = db.ForeignKey(kind="Part")
-	base = db.ForeignKey(kind=Thing) # base Thing OR template
-	template = db.String() # zero.base.torso / templates.whatever / etc
-	material = db.JSON()
-	morphs = db.JSON() # merged into Things.morphs{}
-	opts = db.JSON() # passed to template or merged into Thing.opts{}
-	assets = db.ForeignKey(kind=Asset, repeated=True) # merged into opts
+    parent = db.ForeignKey(kind="Part")
+    base = db.ForeignKey(kind=Thing) # base Thing OR template
+    template = db.String() # zero.base.torso / templates.whatever / etc
+    material = db.JSON()
+    morphs = db.JSON() # merged into Things.morphs{}
+    opts = db.JSON() # passed to template or merged into Thing.opts{}
+    assets = db.ForeignKey(kind=Asset, repeated=True) # merged into opts
 
-	def json(self):
-		d = self.base and self.base.get().json() or {}
-		if "key" in d: # thing key
-			d["thing_key"] = d["key"]
-		d["key"] = self.key.urlsafe()
-		d["template"] = self.template
-		self.material and d["material"].update(self.material)
-		self.morphs and d["morphs"].update(self.morphs)
-		self.opts and d.update(self.opts)
-		for asset in db.get_multi(self.assets):
-			d[asset.name] = asset.item.urlsafe()
-		d["parts"] = [p.json() for p in Part.query(Part.parent == self.key).fetch()]
-		if not self.parent and "name" not in d:
-			d["name"] = "body"
-		return d
+    def json(self):
+        d = self.base and self.base.get().json() or {}
+        if "key" in d: # thing key
+            d["thing_key"] = d["key"]
+        d["key"] = self.key.urlsafe()
+        d["template"] = self.template
+        self.material and d["material"].update(self.material)
+        self.morphs and d["morphs"].update(self.morphs)
+        self.opts and d.update(self.opts)
+        for asset in db.get_multi(self.assets):
+            d[asset.name] = asset.item.urlsafe()
+        d["parts"] = [p.json() for p in Part.query(Part.parent == self.key).fetch()]
+        if not self.parent and "name" not in d:
+            d["name"] = "body"
+        return d
 
 class Person(db.TimeStampedBase):
-	owner = db.ForeignKey(kind=CTUser)
-	body = db.ForeignKey(kind=Part)
-	name = db.String()
-	voice = db.String()
-	mood = db.JSON()
-	vibe = db.JSON()
-	dances = db.JSON()
-	gestures = db.JSON()
-	responses = db.JSON()
+    owner = db.ForeignKey(kind=CTUser)
+    body = db.ForeignKey(kind=Part)
+    name = db.String()
+    voice = db.String()
+    mood = db.JSON()
+    vibe = db.JSON()
+    dances = db.JSON()
+    gestures = db.JSON()
+    responses = db.JSON()
 
-	def json(self):
-		return {
-			"key": self.key.urlsafe(),
-			"name": self.name,
-			"voice": self.voice,
-			"mood": self.mood or {},
-			"vibe": self.vibe or {},
-			"dances": self.dances or {},
-			"gestures": self.gestures or {},
-			"responses": self.responses or {},
-			"body": self.body.get().json()
-		}
+    def json(self):
+        return {
+            "key": self.key.urlsafe(),
+            "name": self.name,
+            "voice": self.voice,
+            "mood": self.mood or {},
+            "vibe": self.vibe or {},
+            "dances": self.dances or {},
+            "gestures": self.gestures or {},
+            "responses": self.responses or {},
+            "body": self.body.get().json()
+        }
 
 class Room(db.TimeStampedBase):
-	owner = db.ForeignKey(kind=CTUser)
-	base = db.ForeignKey(kind=Thing)
-	name = db.String()
-	environment = db.String()
-	material = db.JSON()
-	lights = db.JSON()
-	cameras = db.JSON()
-	opts = db.JSON() # merged into Thing.opts{}
+    owner = db.ForeignKey(kind=CTUser)
+    base = db.ForeignKey(kind=Thing)
+    name = db.String()
+    environment = db.String()
+    material = db.JSON()
+    lights = db.JSON()
+    cameras = db.JSON()
+    opts = db.JSON() # merged into Thing.opts{}
 
-	def json(self):
-		d = self.base and self.base.get().json() or {}
-		self.opts and d.update(self.opts)
-		if "key" in d: # thing key
-			d["thing_key"] = d["key"]
-		d["key"] = self.key.urlsafe()
-		for iname in ["name", "environment", "lights", "cameras"]:
-			item = getattr(self, iname)
-			if item:
-				d[iname] = item
-		d["owner"] = self.owner.urlsafe()
-		self.material and d["material"].update(self.material)
-		d["objects"] = [f.json() for f in Furnishing.query(Furnishing.parent == self.key).fetch()]
-		d["portals"] = [p.data() for p in Portal.query(Portal.target == self.key).fetch()] # incoming
-		return d
+    def json(self):
+        d = self.base and self.base.get().json() or {}
+        self.opts and d.update(self.opts)
+        if "key" in d: # thing key
+            d["thing_key"] = d["key"]
+        d["key"] = self.key.urlsafe()
+        for iname in ["name", "environment", "lights", "cameras"]:
+            item = getattr(self, iname)
+            if item:
+                d[iname] = item
+        d["owner"] = self.owner.urlsafe()
+        self.material and d["material"].update(self.material)
+        d["objects"] = [f.json() for f in Furnishing.query(Furnishing.parent == self.key).fetch()]
+        d["portals"] = [p.data() for p in Portal.query(Portal.target == self.key).fetch()] # incoming
+        return d
 
 class Furnishing(db.TimeStampedBase):
-	parent = db.ForeignKey(kinds=["Room", "Furnishing"])
-	base = db.ForeignKey(kind=Thing)
-	material = db.JSON()
-	opts = db.JSON() # merged into Thing.opts{} - includes pos, rot, etc
+    parent = db.ForeignKey(kinds=["Room", "Furnishing"])
+    base = db.ForeignKey(kind=Thing)
+    material = db.JSON()
+    opts = db.JSON() # merged into Thing.opts{} - includes pos, rot, etc
 
     def rm(self, commit=True, session=session):
-    	portals = self.portals(False)
-    	if portals["outgoing"]:
-    		portals["outgoing"].rm()
-    	for port in portals["incoming"]:
-    		port.rm()
+        portals = self.portals(False)
+        if portals["outgoing"]:
+            portals["outgoing"].rm()
+        for port in portals["incoming"]:
+            port.rm()
         session.delete(self)
         if commit:
             session.commit()
 
     def portals(self, data=True): # portal only
-		portals = { "incoming": Portal.query(Portal.target == self.key).fetch() }
-		if data:
-			portals["incoming"] = [p.data() for p in portals["incoming"]]
-		out = Portal.query(Portal.source == self.key).get()
-		portals["outgoing"] = data and out.data() or out
-		return portals
+        portals = { "incoming": Portal.query(Portal.target == self.key).fetch() }
+        if data:
+            portals["incoming"] = [p.data() for p in portals["incoming"]]
+        out = Portal.query(Portal.source == self.key).get()
+        portals["outgoing"] = data and out.data() or out
+        return portals
 
-	def json(self):
-		d = self.base and self.base.get().json() or {}
-		self.opts and d.update(self.opts)
-		if "key" in d: # thing key
-			d["thing_key"] = d["key"]
-		d["key"] = self.key.urlsafe()
-		d["parent"] = self.parent.urlsafe()
-		self.material and d["material"].update(self.material)
-		d["parts"] = [f.json() for f in Furnishing.query(Furnishing.parent == self.key).fetch()]
-		if d["kind"] == "portal":
-			d["portals"] = self.portals()
-		return d
+    def json(self):
+        d = self.base and self.base.get().json() or {}
+        self.opts and d.update(self.opts)
+        if "key" in d: # thing key
+            d["thing_key"] = d["key"]
+        d["key"] = self.key.urlsafe()
+        d["parent"] = self.parent.urlsafe()
+        self.material and d["material"].update(self.material)
+        d["parts"] = [f.json() for f in Furnishing.query(Furnishing.parent == self.key).fetch()]
+        if d["kind"] == "portal":
+            d["portals"] = self.portals()
+        return d
 
 # should each room have a default incoming portal?
 class Portal(db.TimeStampedBase): # asymmetrical (one per direction)
-	source = db.ForeignKey(kind=Furnishing) # base.kind == "portal"
-	target = db.ForeignKey(kinds=[Furnishing, Room]) # Room -> pending
+    source = db.ForeignKey(kind=Furnishing) # base.kind == "portal"
+    target = db.ForeignKey(kinds=[Furnishing, Room]) # Room -> pending
 
-	def json(self):
-		return self.data()
+    def json(self):
+        return self.data()
