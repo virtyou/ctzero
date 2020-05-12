ff0af9aab9d47e6bd9e83c657f58068bb7742c39
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-12 15:13:36 -0700

    Body: flippage stuff

--- ctzero/js/zero/core/Body.js
+++ ctzero/js/zero/core/Body.js
@@ -19,6 +19,7 @@ zero.core.Body = CT.Class({
 		this.bones[0].position.y = this.head.bones[0].position.y = -12;
 
 		this._.setPositioners();
+		this._.setFlippers();
 		this._.setBounds();
 	},
 	setFriction: function(grippy) {
@@ -47,11 +48,18 @@ zero.core.Body = CT.Class({
 			}
 		});
 	},
+	_applyMod: function(gopts) {
+		for (var op in gopts)
+			for (var dim in gopts[op])
+				this.springs[dim].target = gopts[op][dim];
+	},
 	move: function(ropts) {
+		ropts.body && this._applyMod(ropts.body);
 		this.torso.move(ropts);
 		this.spine.move(ropts.spine);
 	},
 	resize: function(ropts) {
+		ropts.body && this._applyMod(ropts.body);
 		this.torso.resize(ropts);
 		this.spine.resize(ropts.spine);
 	},
@@ -62,13 +70,18 @@ zero.core.Body = CT.Class({
 	energy: function() {
 		return this.person && this.person.energy;
 	},
+	_tickGroup: function() {
+		for (var f in this.flippers)
+			this.group.rotation[f] = this.flippers[f].value;
+		this.group.position.y = this.positioners.bob.value;
+		this.group.position.x = this.positioners.weave.value;
+		this.group.position.z = this.positioners.slide.value;
+	},
 	tick: function() {
 		this.head.tick();
 		this.torso.tick();
 		this.spine.tick();
-		this.group.position.y = this.positioners.bob.value;
-		this.group.position.x = this.positioners.weave.value;
-		this.group.position.z = this.positioners.slide.value;
+		this._tickGroup();
 		zero.core.morphs.tick(this);
 		var skeleton = this.thring.skeleton;
 		this._.customs.forEach(function(c) { c.tick(skeleton); });
