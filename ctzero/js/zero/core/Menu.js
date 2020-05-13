65dbb4ffc75a882aa35e2787f9289f1e131f0cf3
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-12 21:51:10 -0700

    core.Menu fixes - attach to cam

--- ctzero/js/zero/core/Menu.js
+++ ctzero/js/zero/core/Menu.js
@@ -1,25 +1,25 @@
 zero.core.Menu = CT.Class({
 	CLASSNAME: "zero.core.Menu",
 	close: function() {
-		this.opts.scene.remove(this.thring);
 		this.opts.scene.remove(this.group);
 	},
 	init: function(opts) {
-		opts = this.opts = CT.merge(opts, this.opts, {
+		opts = this.opts = CT.merge(opts, {
+			scene: zero.core.camera.get(),
 			depth: 6,
 			width: 3,
 			height: 3,
 			backset: 50,
 			resize: 0.25,
 			color: 0x00ff00
-		});
+		}, this.opts);
 		var bs = opts.backset * 2 / 3,
 			width = camera.width(bs),
 			height = camera.height(bs),
 			w = width / opts.width,
 			h = height / opts.height,
-			cpos = camera.position(),
-			depth = cpos.z - opts.backset,
+//			cpos = camera.position(),
+//			depth = cpos.z - opts.backset,
 			offsetX = width / 2;
 		opts.cubeGeometry = [width, height, opts.depth];
 		opts.material = CT.merge(opts.material, {
@@ -27,23 +27,24 @@ zero.core.Menu = CT.Class({
 			color: opts.color,
 			opacity: 0.2
 		});
-		opts.position = [cpos.x, cpos.y, depth - opts.depth];
+//		opts.position = [cpos.x, cpos.y, depth - opts.depth];
+		opts.position = [0, 0, -(opts.backset + opts.depth)];
 		opts.parts = opts.items.map(function(part, i) {
 			var sel = function() {
 				opts.onselect(part);
-			},  x = cpos.x + (i % opts.width) * w - offsetX,
-				y = cpos.y + Math.floor(i / opts.height) * h;
+			},  x = (i % opts.width) * w - offsetX,
+				y = Math.floor(i / opts.height) * h;
 			return {
 				parts: [
 					CT.merge({
-						position: [x, y, depth],
+						position: [x, y, opts.depth],
 						scale: [opts.resize, opts.resize, opts.resize],
 						onclick: sel
 					}, part),
 					{
 						thing: "Text",
 						text: part.name,
-						position: [x, y, depth],
+						position: [x, y, opts.depth],
 						onclick: sel
 					}
 				]
