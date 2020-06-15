e7f9b107e1a4cb40f94c4e3ee0887a70309eed9e
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-15 15:25:40 -0700

    core loader: added hair import

--- ctzero/js/zero/core.js
+++ ctzero/js/zero/core.js
@@ -33,6 +33,7 @@ CT.require("zero.core.Torso");
 CT.require("zero.core.Head");
 CT.require("zero.core.Body");
 CT.require("zero.core.Custom");
+CT.require("zero.core.hair");
 
 CT.require("zero.core.Brain");
 CT.require("zero.core.Energy");
