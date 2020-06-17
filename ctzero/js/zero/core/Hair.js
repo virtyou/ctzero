dd06c5c98b32ecc86e662f1eea2be48bc8a3d568
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-17 00:20:35 -0700

    Hair: moved Strand args to opts.hair{} (no default)

--- ctzero/js/zero/core/Hair.js
+++ ctzero/js/zero/core/Hair.js
@@ -3,15 +3,11 @@ zero.core.Hair = CT.Class({
 	preassemble: function() {
 		var i, oz = this.opts, pz = oz.parts;
 		for (i = 0; i < oz.count; i++) {
-			pz.push({
+			pz.push(CT.merge(oz.hair, {
 				thing: "Strand",
 				name: "strand" + i,
-				girth: oz.girth,
-				length: oz.length,
-				segments: oz.segments,
-				stiffness: oz.stiffness,
 				position: [oz.space * i, 0, 0]
-			});
+			}));
 		}
 	},
 	tick: function(dts) {
@@ -20,12 +16,8 @@ zero.core.Hair = CT.Class({
 	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(this.opts, opts, {
-			girth: 1,
 			space: 5,
-			count: 5,
-			length: 5,
-			segments: 9,
-			stiffness: 1 // lower is higher
+			count: 5
 		});
 		this.hairs = [];
 	}
