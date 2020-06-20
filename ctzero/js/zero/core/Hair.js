9fa6ac0d7a593de328cbdf54cbd10b6cd575d484
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-19 19:19:26 -0700

    Hair: new constructor opts{} - density, range, offx, and offy (rmed space and count); tick() uses this.count instead of opts.count; preassemble() generates hair strands based on density and range, offset by offx and offz

--- ctzero/js/zero/core/Hair.js
+++ ctzero/js/zero/core/Hair.js
@@ -1,23 +1,33 @@
 zero.core.Hair = CT.Class({
 	CLASSNAME: "zero.core.Hair",
 	preassemble: function() {
-		var i, oz = this.opts, pz = oz.parts;
-		for (i = 0; i < oz.count; i++) {
-			pz.push(CT.merge(oz.hair, {
-				thing: "Strand",
-				name: "strand" + i,
-				position: [oz.space * i, 0, 0]
-			}));
+		var oz = this.opts, r = oz.range,
+			space = r * 2 / oz.density,
+			pz = oz.parts, i = 0,
+			shift = oz.shift, x, z,
+			offx = oz.offx, offz = oz.offz;
+		for (x = -r + offx; x <= r + offx; x += space) {
+			for (z = -r + offz; z <= r + offz; z += space) {
+				pz.push(CT.merge(oz.hair, {
+					thing: "Strand",
+					name: "strand" + i,
+					rotation: [x, 0, z]
+				}));
+				i += 1;
+			}
 		}
+		this.count = i;
 	},
 	tick: function(dts) {
-		for (var i = 0; i < this.opts.count; i++)
+		for (var i = 0; i < this.count; i++)
 			this["strand" + i].tick(dts);
 	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(this.opts, opts, {
-			space: 5,
-			count: 5
+			density: 8,
+			range: Math.PI / 4,
+			offx: -Math.PI / 8,
+			offz: 0
 		});
 		this.hairs = [];
 	}
