584f0aa8bb2bca0678fc0bbffd9efbb69b8aa33b
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-27 15:21:58 -0700

    Pendulum: tick() now does pos/vel stuff (previously in hair); moved old tick() to _tick() (called by tick()) and swapped boost vel influence (x/z)

--- ctzero/js/zero/core/Pendulum.js
+++ ctzero/js/zero/core/Pendulum.js
@@ -1,16 +1,21 @@
 zero.core.Pendulum = CT.Class({
 	CLASSNAME: "zero.core.Pendulum",
-	tick: function(dts, vel) {
+	_tick: function(dts, vel) {
 		var pendz = this.pends, oz = this.opts,
 			damp = oz.damp, vd = oz.veldamp,
 			rot = this.rotation(null, true),
-			dim, pend;
+			dim, pend, other = { x: "z", z: "x" };
 		for (dim of ["x", "z"]) {
 			pend = pendz[dim];
-			pend.boost = (pend.boost + (vel[dim] + vel.y) / vd) * oz.drag;
+			pend.boost = (pend.boost + (vel[other[dim]] + vel.y) / vd) * oz.drag;
 			pend.acceleration = dts * Math.sin(rot[dim]) / damp;
 			this.adjust("rotation", dim, pend.value);
 		};
+	},
+	tick: function(dts) {
+		var pos = this.position(null, true);
+		this.pos && this._tick(dts, this.localize(zero.core.util.vector(this.pos, pos)));
+		this.pos = pos;
 	},
 	setSprings: function() {
 		var oz = this.opts, thaz = this,
