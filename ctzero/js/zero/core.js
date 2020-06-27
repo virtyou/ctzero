70d7999512efc83d240be5027c25f343739a47e4
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-26 18:50:47 -0700

    added Beard import to core loader

--- ctzero/js/zero/core.js
+++ ctzero/js/zero/core.js
@@ -32,6 +32,7 @@ CT.require("zero.core.Leg");
 CT.require("zero.core.Pendulum");
 CT.require("zero.core.Strand");
 CT.require("zero.core.Hair");
+CT.require("zero.core.Beard");
 
 CT.require("zero.core.Torso");
 CT.require("zero.core.Head");
