692e0eefdcd8cf5cd9f091d48d400b8e1ed16cc7
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-27 20:31:53 -0700

    Beard defaults: decreased density 4->3; 6 strand.damp (normal Strand default is 1.4)

--- ctzero/js/zero/core/Beard.js
+++ ctzero/js/zero/core/Beard.js
@@ -2,12 +2,13 @@ zero.core.Beard = CT.Class({
 	CLASSNAME: "zero.core.Beard",
 	init: function(opts) {
 		this.opts = opts = CT.merge(opts, {
-			density: 4,
+			density: 3,
 			position: [0, -5, 8],
 			offx: Math.PI * 4 / 5
 		}, this.opts);
 		opts.strand = CT.merge(opts.strand, {
 			yoff: 3,
+			damp: 6,
 			hard: false
 		});
 	}
