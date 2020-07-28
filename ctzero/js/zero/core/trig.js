ad2cb30f2e498eed9f04f57c8dc74152d438da06
bubbleboy14 <mario.balibrera@gmail.com>
2020-07-27 18:09:28 -0700

    trig: segs() (renamed from amp()) now handles (generates/caches) variable segs; sin60() update (per segs() changes); sin(), which returns one of 628 vals

--- ctzero/js/zero/core/trig.js
+++ ctzero/js/zero/core/trig.js
@@ -1,27 +1,33 @@
 zero.core.trig = {
 	_: {
+		sin: {},
 		amps: {},
 		ratio: 30 / Math.PI
 	},
-	sin: function(angle) {
-		return zero.core.trig.sin60(Math.round(zero.core.trig._.ratio * angle));
+	sin: function(angle, amp) { // 628
+		var segs = zero.core.trig.segs(628, amp),
+			angish = Math.floor(angle * 100),
+			val = segs[Math.abs(angish) % 628];
+		return angish < 0 ? -val : val;
 	},
-	sin60: function(index, amp, talt) {
-		return zero.core.trig.amp(amp)[((index || 0) + (talt || zero.core.util.ticker)) % 60];
+	sin60: function(index, amp, talt) { // full rotation per sec @ 60fps
+		return zero.core.trig.segs(60, amp)[((index || 0) + (talt || zero.core.util.ticker)) % 60];
 	},
-	amp: function(amp) {
+	segs: function(segs, amp) {
 		var _ = zero.core.trig._, inc, i;
-		if (!_.sin) {
-			inc = 1 / _.ratio;
-			_.sin = [];
-			for (i = 0; i < 60; i++)
-				_.sin.push(Math.sin(inc * i));
+		if (!_.sin[segs]) {
+			inc = Math.PI * 2 / segs;
+			_.sin[segs] = [];
+			for (i = 0; i < segs; i++)
+				_.sin[segs].push(Math.sin(inc * i));
 		}
 		if (amp) {
-			if (!_.amps[amp])
-				_.amps[amp] = _.sin.map(v => v * amp);
-			return _.amps[amp];
+			if (!_amps[segs])
+				_.amps[segs] = {};
+			if (!_.amps[segs][amp])
+				_.amps[segs][amp] = _.sin.map(v => v * amp);
+			return _.amps[seg][amp];
 		}
-		return _.sin;
+		return _.sin[segs];
 	}
 };
