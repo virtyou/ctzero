97c9afda72da07df6c3da478c18b52843d719a42
bubbleboy14 <mario.balibrera@gmail.com>
2020-09-07 14:33:07 -0700

    zero.core.Bit w/ init() and tick()

---
+++ ctzero/js/zero/core/Bit.js
@@ -0,0 +1,25 @@
+zero.core.Bit = CT.Class({
+	CLASSNAME: "zero.core.Bit",
+	tick: function(dts) {
+		var oz = this.opts, bz = oz.bounder.bounds,
+			vel = oz.velocity, i = oz.index, wobz = this.wobblers,
+			v, t = zero.core.util.ticker, adjust = this.adjust;
+		["x", "y", "z"].forEach(function(d, i) {
+			v = vel[i];
+			if (wobz[d])
+				v += wobz[d][(t + i) % 60];
+			v && adjust("position", d, v, true);
+		});
+	},
+	init: function(opts) {
+		this.opts = opts = CT.merge(opts, {
+			matcat: "Basic"
+		}, this.opts);
+		var vri, wobz = this.wobblers = {};
+		["x", "y", "z"].forEach(function(d, i) {
+			vri = opts.variance[i];
+			if (vri)
+				wobz[d] = zero.core.trig.segs(60, vri);
+		});
+	}
+}, zero.core.Thing);
