294cad84f06c108500ab2d4c0b84d2804310f9ae
bubbleboy14 <mario.balibrera@gmail.com>
2020-12-10 12:49:50 -0800

    Ramp: support variable tilt

--- ctzero/js/zero/core/Ramp.js
+++ ctzero/js/zero/core/Ramp.js
@@ -1,16 +1,22 @@
 zero.core.Ramp = CT.Class({
 	CLASSNAME: "zero.core.Ramp",
+	_level: Math.PI / 2,
 	getTop: function(pos) {
-		var bz = this.bounds,
+		var rx = this.group.rotation.x,
+			level = this._level,
+			bz = this.bounds,
 			zmin = bz.min.z,
 			zmax = bz.max.z,
 			ymin = bz.min.y,
-			ymax = bz.max.y,
-			zdif = zmax - zmin,
+			ymax = bz.max.y;
+		if (rx == level)
+			return ymin;
+		var zdif = zmax - zmin,
 			ydif = ymax - ymin,
 			zpos = pos.z - zmin,
-			zper = zpos / zdif;
-		return ymin + zper * ydif;
+			zper = zpos / zdif,
+			yper = zper * ydif;
+		return (rx > level) ? (ymax - yper) : (ymin + yper);
 	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(opts, {
