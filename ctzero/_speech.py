cf4c210fba68a28a4eed7e183867e4c8a551aa46
bubbleboy14 <mario.balibrera@gmail.com>
2021-06-05 17:38:54 -0700

    /_speech request handler, chat action: pass identity (acquired via cgi_get(required=False)) to chat()

--- ctzero/_speech.py
+++ ctzero/_speech.py
@@ -4,7 +4,7 @@ from ctzero.speech import chat, say, rec
 def response():
     action = cgi_get("action", choices=["say", "rec", "chat"])
     if action == "chat":
-        succeed(chat(cgi_get("question")))
+        succeed(chat(cgi_get("question"), cgi_get("identity", required=False)))
     language = cgi_get("language")
     voice = cgi_get("voice", default="Joanna")
     if action == "say":
