8e3c9a49aedc98a558bd1d30d07e6adf34d1c931
bubbleboy14 <mario.balibrera@gmail.com>
2021-08-03 19:07:38 -0700

    Arm.build(): generate gesticulation springs and tickers for shoulder and elbow

--- ctzero/js/zero/core/Arm.js
+++ ctzero/js/zero/core/Arm.js
@@ -45,6 +45,16 @@ zero.core.Arm = CT.Class({
 			bonemap: bmap.hand,
 			side: this.opts.side
 		});
+		this.tickers = {};
+		for (var p of ["shoulder", "elbow"]) {
+			var sname = "gesticulate_" + p;
+			this.springs[sname] = zero.core.springController.add({
+				k: 40,
+				damp: 10
+			}, sname, this);
+			this.tickers[sname] = zero.core.tickerController.add(zero.base.tickers.arm[p],
+				sname, this, this.body);
+		}
 	}
 }, zero.core.Skeleton);
 zero.core.Arm.parts = ["clavicle", "shoulder", "elbow", "wrist"];
