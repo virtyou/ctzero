fd1647174ee0da7069659237059714d439e19031
bubbleboy14 <mario.balibrera@gmail.com>
2021-08-25 13:52:02 -0700

    xr: orient() and _.rigidTrans() - both currently disabled (would becalled from launch())

--- ctzero/js/zero/core/xr.js
+++ ctzero/js/zero/core/xr.js
@@ -13,6 +13,9 @@ zero.core.xr = { // https://01.org/blogs/darktears/2019/rendering-immersive-web-
 
 			rnd.render(scene, camera._[view.eye].camera || cam);
 			rnd.clearDepth();
+		},
+		rigidTrans: function(space, pos, rot) {
+			return space.getOffsetReferenceSpace(new XRRigidTransform(pos, rot));
 		}
 	},
 	tick: function(time, frame) {
@@ -37,11 +40,27 @@ zero.core.xr = { // https://01.org/blogs/darktears/2019/rendering-immersive-web-
 		}
 		_.sesh.requestAnimationFrame(zero.core.xr.tick);
 	},
+	orient: function() {
+		var _ = zero.core.xr._, sprz = camera.springs, pos = sprz.position, rot = sprz.rotation;
+		_.space = _.rigidTrans(_.space, {
+			x: pos.x.target,
+			y: pos.y.target,
+			z: pos.z.target
+		}, {
+			x: rot.x.target,
+			y: rot.y.target,
+			z: rot.z.target
+		});
+		CT.log("oriented space");
+	},
 	launch: function(space) {
 		var _ = zero.core.xr._, rnd = _.renderer = new THREE.WebGLRenderer(),
 			ctx = rnd.context, scene = camera.scene, bl;
 		_.space = space;
 		ctx.makeXRCompatible().then(function() {
+
+//			setTimeout(zero.core.xr.orient, 5000);
+
 			bl = _.sesh.baseLayer = new XRWebGLLayer(_.sesh, ctx);
 			_.sesh.updateRenderState({ baseLayer: bl });
 			_.sesh.requestAnimationFrame(zero.core.xr.tick);
