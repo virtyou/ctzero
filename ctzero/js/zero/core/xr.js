b3023e366dcd658c12886e113eacb6c1fdef8ec0
bubbleboy14 <mario.balibrera@gmail.com>
2021-08-25 00:15:33 -0700

    sort of got working w/ sesh.updateRenderState() and disabled fromArray() transformations.......

--- ctzero/js/zero/core/xr.js
+++ ctzero/js/zero/core/xr.js
@@ -4,10 +4,10 @@ zero.core.xr = { // https://01.org/blogs/darktears/2019/rendering-immersive-web-
 			var _ = zero.core.xr._, rnd = _.renderer,
 				scene = camera.scene, cam = camera.get();
 			rnd.setViewport(viewport.x, viewport.y, viewport.width, viewport.height);
-			var viewMatrix = new THREE.Matrix4();
-			viewMatrix.fromArray(viewMatrixArray);
-			cam.projectionMatrix.fromArray(projectionMatrix);
-			cam.matrixWorldInverse.copy(viewMatrix);
+//			var viewMatrix = new THREE.Matrix4();
+//			viewMatrix.fromArray(viewMatrixArray);
+//			cam.projectionMatrix.fromArray(projectionMatrix);
+//			cam.matrixWorldInverse.copy(viewMatrix);
 			scene.updateMatrixWorld(true);
 			rnd.render(scene, cam);
 			rnd.clearDepth();
@@ -16,6 +16,7 @@ zero.core.xr = { // https://01.org/blogs/darktears/2019/rendering-immersive-web-
 	tick: function(time, frame) {
 		var _ = zero.core.xr._, view, viewport,
 			pose = frame.getViewerPose(_.space);
+		CT.log("xr tick!");
 		if (pose) {
 			CT.log("ticking " + pose.views.length + " views");
 			for (view of pose.views) {
@@ -31,12 +32,14 @@ zero.core.xr = { // https://01.org/blogs/darktears/2019/rendering-immersive-web-
 		_.space = space;
 		ctx.makeXRCompatible().then(function() {
 			bl = _.sesh.baseLayer = new XRWebGLLayer(_.sesh, ctx);
+			_.sesh.updateRenderState({ baseLayer: bl });
 			_.sesh.requestAnimationFrame(zero.core.xr.tick);
 			scene.matrixAutoUpdate = false;
 			rnd.autoClear = false;
 			rnd.clear();
 			rnd.setSize(bl.framebufferWidth, bl.framebufferHeight, false);
 			ctx.bindFramebuffer(ctx.FRAMEBUFFER, bl.framebuffer);
+			CT.log("launched!");
 		});
 	},
 	setup: function() {
