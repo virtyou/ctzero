40bb03e2b61d7eebb83d4fd8d28b32e7c5cc8205
bubbleboy14 <mario.balibrera@gmail.com>
2021-08-24 22:56:22 -0700

    xr 1st cut (not working)

---
+++ ctzero/js/zero/core/xr.js
@@ -0,0 +1,61 @@
+zero.core.xr = { // https://01.org/blogs/darktears/2019/rendering-immersive-web-experiences-threejs-webxr
+	_: {
+		eye: function(viewMatrixArray, projectionMatrix, viewport) {
+			var _ = zero.core.xr._, rnd = _.renderer,
+				scene = camera.scene, cam = camera.get();
+			rnd.setViewport(viewport.x, viewport.y, viewport.width, viewport.height);
+			var viewMatrix = new THREE.Matrix4();
+			viewMatrix.fromArray(viewMatrixArray);
+			cam.projectionMatrix.fromArray(projectionMatrix);
+			cam.matrixWorldInverse.copy(viewMatrix);
+			scene.updateMatrixWorld(true);
+			rnd.render(scene, cam);
+			rnd.clearDepth();
+		}
+	},
+	tick: function(time, frame) {
+		var _ = zero.core.xr._, view, viewport,
+			pose = frame.getViewerPose(_.space);
+		if (pose) {
+			CT.log("ticking " + pose.views.length + " views");
+			for (view of pose.views) {
+				viewport = _.sesh.baseLayer.getViewport(view);
+				_.eye(pose.viewMatrix, view.projectionMatrix, viewport);
+			}
+		}
+		_.sesh.requestAnimationFrame(zero.core.xr.tick);
+	},
+	launch: function(space) {
+		var _ = zero.core.xr._, rnd = _.renderer = new THREE.WebGLRenderer(),
+			ctx = rnd.context, scene = camera.scene, bl;
+		_.space = space;
+		ctx.makeXRCompatible().then(function() {
+			bl = _.sesh.baseLayer = new XRWebGLLayer(_.sesh, ctx);
+			_.sesh.requestAnimationFrame(zero.core.xr.tick);
+			scene.matrixAutoUpdate = false;
+			rnd.autoClear = false;
+			rnd.clear();
+			rnd.setSize(bl.framebufferWidth, bl.framebufferHeight, false);
+			ctx.bindFramebuffer(ctx.FRAMEBUFFER, bl.framebuffer);
+		});
+	},
+	setup: function() {
+		var _ = zero.core.xr._;
+		navigator.xr.requestSession('immersive-vr').then(function(sesh) {
+			_.sesh = sesh;
+			sesh.addEventListener("end", () => CT.log("vr session ended"));
+			sesh.requestReferenceSpace("local").then(zero.core.xr.launch);
+		});
+	},
+	init: function() {
+		navigator.xr && navigator.xr.isSessionSupported('immersive-vr').then(function(isit) {
+			isit ? CT.modal.choice({
+				prompt: "enter immersive vr?",
+				data: ["yah", "nah"],
+				cb: function(answer) {
+					(answer == "yah") && zero.core.xr.setup();
+				}
+			}) : CT.log("webxr (immersive-vr) not supported: " + err);
+		});
+	}
+};
