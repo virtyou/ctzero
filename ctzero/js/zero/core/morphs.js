3dd07c129020c14e5d58589907dd6df84ccb967a
bubbleboy14 <mario.balibrera@gmail.com>
2021-12-22 11:32:45 -0800

    morphs: performance enhancements

--- ctzero/js/zero/core/morphs.js
+++ ctzero/js/zero/core/morphs.js
@@ -1,7 +1,8 @@
 zero.core.morphs = {
+	_xyz: ["x", "y", "z"],
 	deprecated: function(thing) { // basic example -- current tick() faster, even on ios
 		var geo = thing.thring.geometry, a, i, val, dim,
-			vert = geo.vertices[0], dims = ["x", "y", "z"],
+			vert = geo.vertices[0], dims = zero.core.morphs._xyz,
 			morphStack = thing.morphStack, base = thing.base;
 		for (i = 0; i < geo.vertices.length * 3; i++) {
 			val = base[i];
@@ -18,7 +19,7 @@ zero.core.morphs = {
 		if (thing.opts.shader)
 			return zero.core.shaders.tick(thing);
 		var geo = thing.thring.geometry, modz = {},
-			dimz = ["x", "y", "z"], a, i, val, stack, base = thing.base;
+			dimz = zero.core.morphs._xyz, a, i, val, stack, base = thing.base;
 		if (thing._upped) {
 			for (a = 0; a < thing._upped.length; a++) {
 				i = thing._upped[a];
@@ -41,33 +42,33 @@ zero.core.morphs = {
 		geo.verticesNeedUpdate = true;
 	},
 	delta: function(thing, a) {
-		var m = thing.morphStack[a],
-			morphz = thing.morphs[a] = {},
+		var diff, i, morphz = thing.morphs[a] = {},
+			m = thing.morphStack[a], base = thing.base,
 			cutoff = core.config.ctzero.morphs.delta_cutoff;
-		thing.base.forEach(function(b, i) {
-			var diff = m[i] - b;
+		for (i = 0; i < base.length; i++) {
+			diff = m[i] - base[i];
 			if (Math.abs(diff) > cutoff)
 				morphz[i] = diff;
-		});
+		}
 	},
 	apply: function(thing) {
-		var i, m, diffz = {}, base = thing.base,
-			cutoff = core.config.ctzero.morphs.delta_cutoff;
+		var stack, degree, diff, diffz = {}, base = thing.base,
+			i, m, cutoff = core.config.ctzero.morphs.delta_cutoff;
 		thing.staticMorphs = [];
 		for (m in thing.morphStack) {
 			if (!thing.morphs[m]) {
 				thing.staticMorphs.push(m);
 				if (thing.opts.skipPrecompile || !thing.opts.morphs[m])
 					continue;
-				var degree = thing.opts.morphs[m],
-					stack = thing.morphStack[m];
-				base.forEach(function(b, i) {
-					var diff = stack[i] - b;
+				degree = thing.opts.morphs[m];
+				stack = thing.morphStack[m];
+				for (i = 0; i < base.length; i++) {
+					diff = stack[i] - base[i];
 					if (Math.abs(diff) > cutoff) {
 						diffz[i] = diffz[i] || 0;
 						diffz[i] += diff * degree;
 					}
-				});
+				}
 			}
 		}
 		for (i in diffz)
