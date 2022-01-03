3d6333f004f55cd2a82535e4c8795b4cdf9652c0
bubbleboy14 <mario.balibrera@gmail.com>
2022-01-03 15:49:09 -0800

    base flora{}: flower{}, bush{}, tree{}

---
+++ ctzero/js/zero/base/flora.js
@@ -0,0 +1,14 @@
+zero.base.flora = {
+	flower: { // stem, leaves, flower[blossom]
+		color: "green"
+	},
+	bush: { // stems, leaves, flower[berry]
+		stems: 10,
+		segments: 3
+	},
+	tree: { // stem[trunk], branches, leaves, flower[fruit]
+		segments: 4,
+		branches: 8,
+		scale: [100, 100, 100]
+	}
+};
