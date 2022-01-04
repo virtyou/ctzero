f232881558a750b1efd41eba97b6984084f8d88b
bubbleboy14 <mario.balibrera@gmail.com>
2022-01-03 20:12:55 -0800

    base.flora{}: flower{} tweakz

--- ctzero/js/zero/base/flora.js
+++ ctzero/js/zero/base/flora.js
@@ -1,12 +1,14 @@
 zero.base.flora = {
-	flower: { // stem, leaves, flower[blossom]
-		color: "green"
+	flower: { // stem, leaves, flower
+		stem: "green",
+		fruits: 0,
+		flowers: 2
 	},
-	bush: { // stems, leaves, flower[berry]
+	bush: { // stems, leaves, fruit[berry]
 		stems: 10,
 		segments: 3
 	},
-	tree: { // stem[trunk], branches, leaves, flower[fruit]
+	tree: { // stem[trunk], branches, leaves, fruit
 		segments: 4,
 		branches: 8,
 		scale: [100, 100, 100]
