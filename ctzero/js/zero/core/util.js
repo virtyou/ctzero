4ecb97f18429eb83bdb7ac1801b18a219b2ec46b
bubbleboy14 <mario.balibrera@gmail.com>
2022-01-10 15:53:50 -0800

    zcu.randHue(): black

--- ctzero/js/zero/core/util.js
+++ ctzero/js/zero/core/util.js
@@ -18,6 +18,8 @@ zero.core.util = {
 	randHue: function(family) {
 		var zcu = zero.core.util, cz = zcu._colors, i2, lz,
 			cstr = "0000", c1, c2, d = CT.data, r = d.random;
+		if (family == "black")
+			return "#" + (10 + r(20)) + "" + (10 + r(20)) + "" + (10 + r(20));
 		if (family == "yellow")
 			return "#" + (80 + r(20)) + "" + (80 + r(20)) + "" + (10 + r(10));
 		if (family == "green") // eh........
