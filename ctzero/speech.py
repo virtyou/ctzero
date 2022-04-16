5bc98fe3ad7a4e94c9517cf644adf3e19d6c9ed2
bubbleboy14 <mario.balibrera@gmail.com>
2022-04-15 20:07:37 -0700

    speech: moved configuration block to spcfg module; trans() derives translatedText from raw response data

--- ctzero/speech.py
+++ ctzero/speech.py
@@ -4,41 +4,11 @@ from cantools.util import read, write, cmd, output
 from cantools.web import log, post, read_file
 from cantools import config
 from model import Translation
+from .spcfg import *
 try:
     from string import letters
 except: # py38
     from string import ascii_letters as letters
-
-LANG = {
-    "english": "en-US",
-    "mandarin": "zh-guoyu"
-}
-CMDS = {
-    "trans": 'gcloud alpha ml translate translate-text --content="%s" --source-language=%s --target-language=%s',
-    "say": [
-        'aws polly synthesize-speech --output-format mp3 --voice-id %s --text "%s" %s.mp3',
-        'aws polly synthesize-speech --output-format json --voice-id %s --text "%s" --speech-mark-types=\'["viseme"]\' %s.json'
-    ],
-    "rec": {
-        "transcode": config.ctzero.asr.audlib + " -i %s.webm -ac 1 %s.wav", # stampath, stampath
-        "interpret": {
-            "gcloud": "gcloud ml speech recognize %s.wav --language-code=%s", # stampath, lang
-            "baidu": 'wget --header="Content-Type: audio/wav;rate=16000" --post-file=%s.wav "http://vop.baidu.com/server_api?lan=%s&cuid=10569015&token=%s" -O %s.json' # stampath, lang, token, stampath
-        }
-    },
-    "baidu_token": 'wget "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s&" -O baidu.token' # id, secret
-}
-AIZ = {
-    "aiio": {
-        "host": "ai.samsaravr.io",
-        "path": "_respond",
-        "proto": "https"
-    },
-    "pandorabots": {
-        "host": "aiaas.pandorabots.com"
-    }
-}
-BAIDU_TOKEN_LIFE = 2592000
 
 def load_token(now):
     cfg = config.ctzero.asr
@@ -62,7 +32,8 @@ def trans(words, sourceLang, targetLang):
     t = Translation.query(Translation.words == words,
         Translation.source == sourceLang, Translation.target == targetLang).get()
     if not t:
-        res = output(CMDS["trans"]%(words, sourceLang, targetLang))
+        raw = output(CMDS["trans"]%(words, sourceLang, targetLang))
+        res = json.loads(raw)["data"]["translations"][0]["translatedText"]
         t = Translation(words=words, source=sourceLang, target=targetLang, result=res)
         t.put()
     return t.result
