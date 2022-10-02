c7ca6b5c2ff4ec610d545f8bdc0771107cb7c307
bubbleboy14 <mario.balibrera@gmail.com>
2022-10-01 18:13:25 -0700

    Garment: setTexture() and preassemble() stuff for texturing compound garments; reorder constructor merge()

--- ctzero/js/zero/core/Garment.js
+++ ctzero/js/zero/core/Garment.js
@@ -1,11 +1,19 @@
 zero.core.Garment = CT.Class({
 	CLASSNAME: "zero.core.Garment",
 	preassemble: function() {
-
+		var i, oz = this.opts, pz = oz.parts;
+		if (!oz.texture) return;
+		for (i = 0; i < pz.length; i++)
+			pz[i].texture = oz.texture;
+	},
+	setTexture: function(tx) {
+		var uobj = { texture: tx };
+		this.update(uobj);
+		this.parts.forEach(p => p.update(uobj));
 	},
 	init: function(opts) {
 		var bodpart = this.opts.bodpart = this.opts.kind.split("_").pop();
-		this.opts = opts = CT.merge(opts, zero.base.clothes[bodpart], {
+		this.opts = opts = CT.merge(zero.base.clothes[bodpart][this.opts.name], opts, {
 
 		}, this.opts);
 	}
