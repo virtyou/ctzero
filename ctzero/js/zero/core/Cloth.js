fc3aea47c5a48f9ad7034ce65bd6c08cd1d65bfa
bubbleboy14 <mario.balibrera@gmail.com>
2022-12-19 16:18:25 -0800

    Cloth constructor: restored original (example-derived) width/height defaults; opts{} fallbacks - bufferGeometry true, material{side:THREE.DoubleSide}, and disabled rotation[]

--- ctzero/js/zero/core/Cloth.js
+++ ctzero/js/zero/core/Cloth.js
@@ -9,8 +9,8 @@ zero.core.Cloth = CT.Class({
 		this.softBody = zero.core.ammo.softBody(this, oz.frame, oz.anchorPoints);
 	},
 	init: function(opts) { // should translate geometry?
-		opts.width = opts.width || 16;
-		opts.height = opts.height || 12;
+		opts.width = opts.width || 4;
+		opts.height = opts.height || 3;
 		opts.numSegsZ = opts.width * 5;
 		opts.numSegsY = opts.height * 5;
 		// meh? (frame/scene below)
@@ -18,8 +18,12 @@ zero.core.Cloth = CT.Class({
 		opts.scene = zero.core.camera.scene;
 		this.opts = opts = CT.merge(opts, {
 			anchorPoints: "ends",
-			rotation: [0, Math.PI * 0.5, 0],
-			planeGeometry: [opts.width, opts.height, opts.numSegsZ, opts.numSegsY]
+			bufferGeometry: true,
+//			rotation: [0, Math.PI * 0.5, 0],
+			planeGeometry: [opts.width, opts.height, opts.numSegsZ, opts.numSegsY],
+			material: {
+				side: THREE.DoubleSide
+			}
 		}, this.opts);
 	}
 }, zero.core.Thing);
