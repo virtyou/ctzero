e71e72c70b84592f4cfcdc426f33433f766e760e
bubbleboy14 <mario.balibrera@gmail.com>
2022-12-28 14:13:56 -0800

    ammo: various consts

--- ctzero/js/zero/core/ammo.js
+++ ctzero/js/zero/core/ammo.js
@@ -31,7 +31,7 @@ zero.core.ammo = {
 				colShape = new Ammo.btBoxShape(ammo.vector(s.x / 2, s.y / 2, s.z / 2)); // TODO: other shapes...
 			colShape.setMargin(_.consts.margin);
 			colShape.calculateLocalInertia(mass, localInertia);
-			let rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, colShape, localInertia),
+			const rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, colShape, localInertia),
 				body = new Ammo.btRigidBody(rbInfo);
 			mass && body.setActivationState(_.STATE.DISABLE_DEACTIVATION);
 			_.physicsWorld.addRigidBody(body);
@@ -51,25 +51,26 @@ zero.core.ammo = {
 		CT.data.remove(zero.core.ammo._.kinematics, k);
 	},
 	tick: function(dts) {
-		let _ = zero.core.ammo._, k, s, r;
+		const ammo = zero.core.ammo, _ = ammo._;
 		if (!_.softs.length && !_.kinematics.length) return;
+		let k, s, r;
 		for (k of _.kinematics)
-			zero.core.ammo.tickKinematic(k, dts);
+			ammo.tickKinematic(k, dts);
 		_.physicsWorld.stepSimulation(zero.core.util.dt, 10);
 		for (s of _.softs)
-			zero.core.ammo.tickSoft(s, dts);
+			ammo.tickSoft(s, dts);
 		for (r of _.rigids)
-			zero.core.ammo.tickRigid(r, dts);
+			ammo.tickRigid(r, dts);
 	},
 	tickSoft: function(s, dts) {
-		var geo = s.geometry,
+		const geo = s.geometry,
 			attrs = geo.attributes,
 			pos = attrs.position,
 			positions = pos.array,
 			numVerts = positions.length / 3,
-			nodes = s.userData.physicsBody.get_m_nodes(),
-			ifloat = 0, node, nodePos;
-		for (let i = 0; i < numVerts; i++) {
+			nodes = s.userData.physicsBody.get_m_nodes();
+		let i, ifloat = 0, node, nodePos;
+		for (i = 0; i < numVerts; i++) {
 			node = nodes.at(i);
 			nodePos = node.get_m_x();
 			positions[ifloat++] = nodePos.x();
@@ -86,58 +87,59 @@ zero.core.ammo = {
 		ms && ms.setWorldTransform(ammo.kinemaTrans(k));
 	},
 	tickRigid: function(r, dts) {
-		let _ = zero.core.ammo._,
+		const _ = zero.core.ammo._,
 			ms = r.userData.physicsBody.getMotionState();
 		if (ms) {
 			ms.getWorldTransform(_.transformer);
-			const p = _.transformer.getOrigin();
-			const q = _.transformer.getRotation();
+			const p = _.transformer.getOrigin(),
+				q = _.transformer.getRotation();
 			r.position.set(p.x(), p.y(), p.z());
 			r.quaternion.set(q.x(), q.y(), q.z(), q.w());
 		}
 	},
 	rigid: function(mass, p, s, q, mat) { // creates thring
-		let _ = zero.core.ammo._, thring;
+		const _ = zero.core.ammo._;
 		q = q || _.defQuat;
 		mat = mat || new THREE.MeshPhongMaterial( { color: 0xFFFFFF } );
 		_.positioner.set(p.x, p.y, p.z);
 		_.quatter.set(q.x, q.y, q.z, q.w);
-		thring = new THREE.Mesh(new THREE.BoxGeometry(s.x, s.y, s.z, 1, 1, 1), mat);
+		const thring = new THREE.Mesh(new THREE.BoxGeometry(s.x, s.y, s.z, 1, 1, 1), mat);
 		thring.userData.physicsBody = _.rigid(s, mass);
 		zero.core.camera.scene.add(thring);
 		mass && _.rigids.push(thring);
 		return thring;
 	},
 	kinematic: function(thring) { // adapts thring
-		let _ = zero.core.ammo._, body;
+		const _ = zero.core.ammo._;
 		thring.getWorldPosition(_.positioner);
 		thring.getWorldQuaternion(_.quatter);
 
-		body = _.rigid(thring.scale, 1); // FIX: thring.scale seems wrong......
+		const body = _.rigid(thring.scale, 1); // FIX: thring.scale seems wrong......
 
 		body.setCollisionFlags(_.FLAGS.CF_KINEMATIC_OBJECT);
 		thring.userData.physicsBody = body;
 		_.kinematics.push(thring);
 	},
 	hinge: function(r1, r2, p1, p2, axis) {
-		let ammo = zero.core.ammo, hinge;
+		const ammo = zero.core.ammo;
 		axis = axis || ammo.vector(0, 1, 0);
-		hinge = new Ammo.btHingeConstraint(r1.userData.physicsBody,
+		const hinge = new Ammo.btHingeConstraint(r1.userData.physicsBody,
 			r2.userData.physicsBody, p1, p2, axis, axis, true);
 		ammo._.physicsWorld.addConstraint(hinge, true);
 		return hinge;
 	},
 	softBody: function(cloth, anchor, anchorPoints) {
-		let ammo = zero.core.ammo, _ = ammo._, consts = _.consts,
+		const ammo = zero.core.ammo, _ = ammo._, consts = _.consts,
 			coz = cloth.opts, width = coz.width, height = coz.height,
-			i, anx, pos = cloth.position(null, true);
-		const c00 = ammo.vector(pos.x, pos.y + height, pos.z);
-		const c01 = ammo.vector(pos.x, pos.y + height, pos.z - width);
-		const c10 = ammo.vector(pos.x, pos.y, pos.z);
-		const c11 = ammo.vector(pos.x, pos.y, pos.z - width);
-		const softBody = _.softBodyHelpers.CreatePatch(_.physicsWorld.getWorldInfo(),
-			c00, c01, c10, c11, coz.numSegsZ + 1, coz.numSegsY + 1, 0, true);
-		const sbcfg = softBody.get_m_cfg();
+			pos = cloth.position(null, true),
+			c00 = ammo.vector(pos.x, pos.y + height, pos.z),
+			c01 = ammo.vector(pos.x, pos.y + height, pos.z - width),
+			c10 = ammo.vector(pos.x, pos.y, pos.z),
+			c11 = ammo.vector(pos.x, pos.y, pos.z - width),
+			softBody = _.softBodyHelpers.CreatePatch(_.physicsWorld.getWorldInfo(),
+				c00, c01, c10, c11, coz.numSegsZ + 1, coz.numSegsY + 1, 0, true),
+			sbcfg = softBody.get_m_cfg();
+
 		sbcfg.set_viterations(10);
 		sbcfg.set_piterations(10);
 
@@ -151,11 +153,11 @@ zero.core.ammo = {
 		_.softs.push(cloth.thring);
 		if (anchor) {
 			anchor.userData.physicsBody || ammo.kinematic(anchor);
-			const abod = anchor.userData.physicsBody;
+			const abod = anchor.userData.physicsBody,
+				anx = [];
 			anchorPoints = anchorPoints || "ends";
-			anx = [];
 			if (anchorPoints == "full")
-				for (i = 0; i <= coz.numSegsZ; i++)
+				for (let i = 0; i <= coz.numSegsZ; i++)
 					anx.push(i);
 			else if (anchorPoints == "ends") {
 				anx.push(0);
@@ -197,7 +199,7 @@ zero.core.ammo = {
 		return ammo.transform(_.transformer, _.aPositioner, _.aQuatter);
 	},
 	load: function(AmmoLib) {
-		let ammo = zero.core.ammo, _ = ammo._;
+		const ammo = zero.core.ammo, _ = ammo._;
 		Ammo = AmmoLib;
 
 		_.collisionConfiguration = new Ammo.btSoftBodyRigidBodyCollisionConfiguration();
@@ -221,7 +223,7 @@ zero.core.ammo = {
 		_.onload && _.onload();
 	},
 	init: function(onload) {
-		let ammo = zero.core.ammo;
+		const ammo = zero.core.ammo;
 		ammo._.onload = onload;
 		Ammo().then(ammo.load);
 	}
