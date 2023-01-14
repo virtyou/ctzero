5dba075dc7dd283246e82b9bac56ad548d35250a
bubbleboy14 <mario.balibrera@gmail.com>
2023-01-14 00:56:33 -0800

    Garment: preassemble() sets onbuild()s (kindeBody()) per rigids[]; constructor - rigids[] fallback; various consts/lets

--- ctzero/js/zero/core/Garment.js
+++ ctzero/js/zero/core/Garment.js
@@ -1,22 +1,27 @@
 zero.core.Garment = CT.Class({
 	CLASSNAME: "zero.core.Garment",
 	preassemble: function() {
-		var i, oz = this.opts, pz = oz.parts;
+		const oz = this.opts, pz = oz.parts,
+			rz = oz.rigids, ammo = zero.core.ammo;
+		let i, p;
 		for (i = 0; i < pz.length; i++) {
-			pz[i].garment = this;
+			p = pz[i];
+			p.garment = this;
 			if (oz.texture)
-				pz[i].texture = oz.texture;
+				p.texture = oz.texture;
+			if (rz.includes(p.name))
+				p.onbuild = (thing) => ammo.kineBody(thing.thring);
 		}
 	},
 	setTexture: function(tx) {
-		var uobj = { texture: tx };
+		const uobj = { texture: tx };
 		this.update(uobj);
 		this.parts.forEach(p => p.update(uobj));
 	},
 	init: function(opts) {
-		var bodpart = this.opts.bodpart = this.opts.kind.split("_").pop();
+		const bodpart = this.opts.bodpart = this.opts.kind.split("_").pop();
 		this.opts = opts = CT.merge(zero.base.clothes[bodpart][this.opts.name], opts, {
-
+			rigids: []
 		}, this.opts);
 	}
 }, zero.core.Thing);
