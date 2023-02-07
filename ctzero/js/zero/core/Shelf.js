8c2fc2309d6b2d7e364410fe97b8468e10628cdd
bubbleboy14 <mario.balibrera@gmail.com>
2023-02-07 05:54:51 -0800

    Shelf.preassemble(): support back and sides

--- ctzero/js/zero/core/Shelf.js
+++ ctzero/js/zero/core/Shelf.js
@@ -3,12 +3,10 @@ zero.core.Shelf = CT.Class({
 	preassemble: function() {
 		const oz = this.opts, pz = oz.parts,
 			w = oz.width, d = oz.depth, l = oz.legs,
-			wh = w / 2, dh = d / 2,
-			ll = l.length, lw = l.width;
-		let lfl = ll, llh;
-		if (l.full)
-			lfl += (oz.levels - 1) * oz.spacing;
-		llh = lfl / 2;
+			wh = w / 2, dh = d / 2, ll = l.length, lw = l.width,
+			sheight = (oz.levels - 1) * oz.spacing,
+			lfl = l.full ? (ll + sheight) : ll,
+			llh = lfl / 2;
 		pz.push({
 			name: "leg1",
 			kind: "leg",
@@ -33,6 +31,28 @@ zero.core.Shelf = CT.Class({
 			boxGeometry: [lw, lfl, lw],
 			position: [-wh, llh, -dh]
 		});
+		if (oz.back) {
+			pz.push({
+				name: "back",
+				kind: "side",
+				boxGeometry: [w, sheight, 2],
+				position: [0, ll + sheight / 2, -dh]
+			});
+		}
+		if (oz.sides) {
+			pz.push({
+				name: "left",
+				kind: "side",
+				boxGeometry: [2, sheight, d],
+				position: [-wh, ll + sheight / 2, 0]
+			});
+			pz.push({
+				name: "right",
+				kind: "side",
+				boxGeometry: [2, sheight, d],
+				position: [wh, ll + sheight / 2, 0]
+			});
+		}
 		for (let i = 0; i < oz.levels; i++) {
 			pz.push({
 				name: "level" + i,
