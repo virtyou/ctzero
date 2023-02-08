cfc7076c6e1f5c7a60e46ef17c319c62981e6dbd
bubbleboy14 <mario.balibrera@gmail.com>
2023-02-08 00:46:53 -0800

    zero.base.carpentry{}: table, desk, chair, cabinet, bookshelf

---
+++ ctzero/js/zero/base/carpentry.js
@@ -0,0 +1,43 @@
+zero.base.carpentry = {
+	table: {
+		// defaults
+	},
+	desk: {
+		width: 50,
+		depth: 30,
+		levels: 2,
+		spacing: 10,
+		thickness: 2,
+		back: "tall",
+		sides: true
+	},
+	chair: {
+		width: 20,
+		depth: 20,
+		spacing: 30,
+		thickness: 2,
+		back: "tall",
+		legs: {
+			width: 2
+		}
+	},
+	cabinet: {
+		back: true,
+		sides: true,
+		levels: 5,
+		depth: 30,
+		thickness: 1,
+		legs: {
+			length: 0,
+			full: false
+		}
+	},
+	bookshelf: {
+		width: 120,
+		depth: 20,
+		levels: 4,
+		legs: {
+			length: 0
+		}
+	}
+};
