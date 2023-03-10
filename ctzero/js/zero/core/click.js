0cd8f3057db7747d189f16fb7027bb5070d98c87
bubbleboy14 <mario.balibrera@gmail.com>
2023-03-09 18:07:28 -0800

    core.click.init(): updated from projector.unprojectVector(vector, cam) to vector.unproject(cam); rmed (now-unused) Projector initialization

--- ctzero/js/zero/core/click.js
+++ ctzero/js/zero/core/click.js
@@ -4,17 +4,16 @@ zero.core.click = {
         // adapted from: https://stemkoski.github.io/Three.js/Mouse-Click.html
         if (zero.core.click._ready) return;
         zero.core.click._ready = true;
-        var mouse = {}, projector = new THREE.Projector(),
-            cam = zero.core.camera.get(), vector, ray, intersects, i,
+        var mouse = {}, cam = zero.core.camera.get(),
             can = document.getElementsByTagName("canvas")[0],
-            offset = CT.align.offset(can);
+            offset = CT.align.offset(can), vector, ray, intersects, i;
         can.parentNode.addEventListener(CT.info.mobile
             ? 'touchstart' : 'mousedown', function(e) {
             mouse.x = ((e.clientX - offset.left) / can.clientWidth) * 2 - 1;
             mouse.y = -((e.clientY - offset.top) / can.clientHeight) * 2 + 1;
 
             vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
-            projector.unprojectVector(vector, cam);
+            vector.unproject(cam);
             ray = new THREE.Raycaster(cam.position, vector.sub(cam.position).normalize());
             intersects = ray.intersectObjects(zero.core.click.targets, true);
 
