8489bfd4fbdb0405e8c8f191facf8c3304eea063
bubbleboy14 <mario.balibrera@gmail.com>
2023-03-10 22:44:14 -0800

    base.particles: added various alphaTest rules

--- ctzero/js/zero/base/particles.js
+++ ctzero/js/zero/base/particles.js
@@ -4,6 +4,7 @@ zero.base.particles = {
 		variance: [1, 0, 1],
 		pmat: {
 			opacity: 0.3,
+			alphaTest: 0.3,
 			shininess: 150,
 			color: 0x22ccff,
 			transparent: true
@@ -17,6 +18,7 @@ zero.base.particles = {
 		dissolve: 0.5,
 		pmat: {
 			opacity: 0,
+			alphaTest: 0.1,
 			shininess: 150,
 			color: 0x22ccff,
 			transparent: true
@@ -33,6 +35,7 @@ zero.base.particles = {
 		drip: true,
 		pmat: {
 			opacity: 0,
+			alphaTest: 0.1,
 			shininess: 150,
 			color: 0xff2222,
 			transparent: true
@@ -48,6 +51,7 @@ zero.base.particles = {
 		drip: true,
 		pmat: {
 			opacity: 0,
+			alphaTest: 0.1,
 			color: 0x888888,
 			transparent: true,
 			side: THREE.BackSide
