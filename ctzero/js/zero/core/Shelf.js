e165f7af729d3eb300b18df3e08d9d994f066087
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-06 15:54:23 -0700

    Shelf: initPart() and initSide() support parts[] (with this.opts.parts[] fallback) arg; setDrawer() and getCompartment(); extracted initLevel() from initLevels(), and added drawer detection (which triggers setDrawer() call); openable(), opener(), open(), and close(); added front default (false) to constructor opts{}

--- ctzero/js/zero/core/Shelf.js
+++ ctzero/js/zero/core/Shelf.js
@@ -1,13 +1,15 @@
 zero.core.Shelf = CT.Class({
 	CLASSNAME: "zero.core.Shelf",
-	initPart: function(name, kind, geo, pos) {
-		this.opts.parts.push({
+	initPart: function(name, kind, geo, pos, parts) {
+		const popts = {
 			name: name,
 			kind: kind,
-			matinstance: this.material,
+			position: pos,
 			boxGeometry: geo,
-			position: pos
-		});
+			matinstance: this.material
+		};
+		(parts || this.opts.parts).push(popts);
+		return popts;
 	},
 	initLegs: function(girth, height, x, y, z) {
 		for (let i = 0; i < 4; i++) {
@@ -18,15 +20,30 @@ zero.core.Shelf = CT.Class({
 				z *= -1;
 		}
 	},
-	initSide: function(name, geo, pos) {
-		this.initPart(name, "side", geo, pos);
+	initSide: function(name, geo, pos, parts) {
+		return this.initPart(name, "side", geo, pos, parts);
+	},
+	initLevel: function(i, voff) {
+		const oz = this.opts, w = oz.width, d = oz.depth, dz = oz.drawers,
+			t = oz.thickness, s = oz.spacing, ll = oz.legs.length,
+			popts = this.initPart("level" + (i + 1), "level",
+				[w, t, d], [0, ll + s * i - voff, 0]);
+		dz && dz > i && this.setDrawer(popts);
 	},
 	initLevels: function(voff) {
-		const oz = this.opts, pz = oz.parts, s = oz.spacing,
-			w = oz.width, t = oz.thickness, d = oz.depth, ll = oz.legs.length;
-		for (let i = 0; i < oz.levels; i++)
-			this.initPart("level" + (i + 1), "level",
-				[w, t, d], [0, ll + s * i - voff, 0]);
+		for (let i = 0; i < this.opts.levels; i++)
+			this.initLevel(i, voff);
+	},
+	setDrawer: function(popts) {
+		var oz = this.opts, t = oz.thickness, outer;
+		popts.parts = [];
+		popts.isDrawer = true;
+		this.drawers.push(popts.name);
+		outer = this.initSide("outer", [oz.width, oz.spacing, t],
+			[0, oz.spacing / 2, oz.depth / 2], popts.parts);
+		outer.parts = [];
+		this.initPart("handle", "attachment",
+			[oz.width / 4, t, t], [0, 0, t], outer.parts);
 	},
 	preassemble: function() {
 		const oz = this.opts, pz = oz.parts, tx = oz.texture,
@@ -42,6 +59,7 @@ zero.core.Shelf = CT.Class({
 			fronty = ll + fheight / 2 - voff,
 			sidey = ll + sheight / 2 - voff;
 		this.material = this.getMaterial();
+		this.drawers = [];
 		this.initLegs(lw, lfl, wh, legy, dh);
 		oz.back && this.initSide("back", [w, bheight, 2], [0, backy, -dh]);
 		oz.front && this.initSide("front", [w, fheight, 2], [0, fronty, dh]);
@@ -57,6 +75,45 @@ zero.core.Shelf = CT.Class({
 	},
 	postassemble: function() {
 		this.opts.items.forEach(this.placeItem);
+	},
+	openable: function() {
+		return !!(this.drawers.length || this.lid);
+	},
+	opener: function() {
+		const options = this.drawers.slice();
+		this.lid && options.unshift("lid");
+		if (!options.length)
+			return this.log("nothing to open!");
+		if (options.length == 1)
+			return this.open(options.pop());
+		CT.moda.choice({
+			prompt: "want to open this " + this.opts.variety + "?",
+			data: options,
+			cb: this.open
+		});
+	},
+	open: function(compartment) {
+		compartment = this.getCompartment(compartment);
+		if (!compartment || compartment._opened) return this.log("nothing to open!");
+		compartment._opened = true;
+		if (compartment.opts.isDrawer)
+			compartment.adjust("position", "z", this.opts.depth, true);
+		else { // lid
+
+		}
+	},
+	close: function(compartment) {
+		compartment = this.getCompartment(compartment);
+		if (!compartment || !compartment._opened) return this.log("nothing to close!");
+		compartment._opened = false;
+		if (compartment.opts.isDrawer)
+			compartment.adjust("position", "z", -this.opts.depth, true);
+		else { // lid
+
+		}
+	},
+	getCompartment: function(compartment) {
+		return this[compartment] || this.lid || (this.drawers[0] && this[this.drawers[0]]);
 	},
 	placeItem: function(item, i) {
 		const oz = this.opts;
@@ -108,8 +165,9 @@ zero.core.Shelf = CT.Class({
 			depth: 40,
 			spacing: 20,
 			thickness: 4,
-			back: false, // |true|tall
-			sides: false,
+			sides: false, // |true|tall
+			front: false, // |true|tall
+			back: false,  // |true|tall
 			items: []
 		}, this.opts);
 	}
