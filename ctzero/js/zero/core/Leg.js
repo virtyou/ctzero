d1b0381f288edec4becd09771f26e98b9d81c170
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-09 16:33:54 -0700

    Leg: kick() and unkick() (and _kicker{} and _unkicker{})

--- ctzero/js/zero/core/Leg.js
+++ ctzero/js/zero/core/Leg.js
@@ -1,5 +1,12 @@
 zero.core.Leg = CT.Class({
 	CLASSNAME: "zero.core.Leg",
-	// gotta be some overrides :)
+	_kicker: {hip: {x: -2}},
+	_unkicker: {hip: {x: 0}},
+	kick: function() {
+		this.setSprings(this._kicker);
+	},
+	unkick: function() {
+		this.setSprings(this._unkicker);
+	}
 }, zero.core.Skeleton);
 zero.core.Leg.parts = ["hip", "knee", "ankle", "toe"];
