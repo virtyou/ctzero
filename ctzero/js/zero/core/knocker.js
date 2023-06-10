5bb2895f0176204871f04ed6590ae2c58c6afaef
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-09 17:13:52 -0700

    knocker: foot/kick -related stuff

--- ctzero/js/zero/core/knocker.js
+++ ctzero/js/zero/core/knocker.js
@@ -16,6 +16,9 @@ zero.core.knocker = {
 		hand: function(prey, knocker) {
 			prey.knock(zero.core.util.charDir(), null, 300);
 		},
+		foot: function(prey, knocker) {
+			prey.knock(zero.core.util.charDir(), null, 500);
+		},
 		knocker: function(prey, knocker) {
 			var zc = zero.core;
 			prey.knock(zc.util.charDir(), zc.knocker._.crash);
@@ -28,7 +31,7 @@ zero.core.knocker = {
 	log: function(msg) {
 		CT.log("knocker: " + msg);
 	},
-	hit: function(striker, preykinds, hitter, cfg, nohit, onhit, isheld) {
+	hit: function(striker, preykinds, hitter, cfg, nohit, onhit, isheld, glopo) {
 		var zc = zero.core, touching = zc.util.touching, zcc = zc.current,
 			men = zcc.room.menagerie, source, sb, pk, p, prey, sfx, hitting;
 		for (pk of preykinds) {
@@ -37,7 +40,7 @@ zero.core.knocker = {
 				sb = source && zcc.people[source].body;
 				for (p in men[pk]) {
 					prey = men[p];
-					if (touching(striker, prey, 50, isheld, isheld)) {
+					if (touching(striker, prey, 50, isheld, isheld || glopo)) {
 						hitting = true;
 						if (hitter(prey)) {
 							prey.repos(sb && sb.position(), true);
@@ -59,14 +62,22 @@ zero.core.knocker = {
 			preykinds, onsplat, splatcfg, nosplat);
 	},
 	strike: function(prey, striker) {
-		var k = zero.core.knocker, sname = striker.name,
-			stype = striker.opts.variety || "knocker";
-		if (!sname) {
+		var k = zero.core.knocker, sname = striker.name, stype;
+		if (sname == "foot") {
+			stype = "foot";
+			sname = "kick";
+		} else if (!sname) {
 			stype = "hand";
 			sname = striker.opts.side;
-		}
+		} else
+			stype = striker.opts.variety || "knocker";
 		k.log(prey.name + " struck by " + sname + " (" + stype + ")");
 		k.strikers[stype](prey, striker);
+	},
+	kick: function(preykinds, onknock, knockcfg, side) {
+		var zc = zero.core, k = zc.knocker, striker = zc.current.person.body.torso.legs[side].foot;
+		return zc.knocker.hit(striker, preykinds, prey => onknock(prey, side),
+			knockcfg, null, prey => k.strike(prey, striker), false, true);
 	},
 	knock: function(preykinds, onknock, knockcfg, side) {
 		var zc = zero.core, k = zc.knocker, striker = zc.current.person.held(side, true);
