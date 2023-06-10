d8153f6feaba072779f1544449d4c4fb75e407bf
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-09 17:12:07 -0700

    Leg.foot{} w/: name, radii{}, position()

--- ctzero/js/zero/core/Leg.js
+++ ctzero/js/zero/core/Leg.js
@@ -2,6 +2,13 @@ zero.core.Leg = CT.Class({
 	CLASSNAME: "zero.core.Leg",
 	_kicker: {hip: {x: -2}},
 	_unkicker: {hip: {x: 0}},
+	foot: {
+		name: "foot",
+		radii: { x: 10, y: 10, z: 10 }, // HACKY :'(
+		position: function(ignoredPos, world) {
+			return zero.core.util.getPos(this.toe, world);
+		}
+	},
 	kick: function() {
 		this.setSprings(this._kicker);
 	},
