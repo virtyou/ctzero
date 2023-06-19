d7632b59de0f048e764c0ed34cf10e00be9b00d3
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-19 15:26:24 -0700

    base.items, extracted from base.clothes (held)

---
+++ ctzero/js/zero/base/items.js
@@ -0,0 +1,69 @@
+var P2 = Math.PI / 2, P4 = P2 / 2, C4 = ["curve4", 3, 0.5, 3];
+
+zero.base.items = {
+	torch: {
+		cylinderGeometry: true,
+		geomult: 60,
+		rotation: [P2, 0, 0],
+		position: [0, -8, 25],
+		material: {
+			color: "#4a3728"
+		},
+		parts: [{
+			thing: "Fire",
+			regTick: true,
+			faceUp: true,
+			position: [0, 30, 0],
+			scale: [0.5, 0.5, 0.5]
+		}]
+	},
+	mallet: {
+		variety: "smasher",
+		cylinderGeometry: 2,
+		geomult: 30,
+		rotation: [P2, 0, 0],
+		position: [0, -8, 25],
+		material: {
+			color: "#4a3728"
+		},
+		parts: [{
+			name: "head",
+			cylinderGeometry: 10,
+			position: [0, 30, 0],
+			rotation: [P2, 0, 0],
+			material: {
+				color: "#00ff00"
+			}
+		}]
+	},
+	"lacrosse stick": {
+		variety: "grabber",
+		cylinderGeometry: true,
+		geomult: 60,
+		rotation: [P2, 0, 0],
+		position: [0, -8, 25],
+		material: {
+			color: "#0000ff"
+		},
+		parts: [{
+			name: "perch",
+			position: [0, 30, 0],
+			parts: [{
+				tubeGeometry: C4,
+				rotation: [0, -P2, 0]
+			}, {
+				tubeGeometry: C4,
+				rotation: [0, -P4, 0]
+			}, {
+				tubeGeometry: C4,
+				rotation: [0, 0, 0]
+			}, {
+				tubeGeometry: C4,
+				rotation: [0, P4, 0]
+			}, {
+				tubeGeometry: C4,
+				rotation: [0, P2, 0]
+			}]
+		}]
+	}
+};
