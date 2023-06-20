7b7ab676931941fb13c14f8969e7f94e69571238
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-20 15:36:00 -0700

    Item: _.wreck(); smash(), melt(), burn()

--- ctzero/js/zero/core/Item.js
+++ ctzero/js/zero/core/Item.js
@@ -1,6 +1,26 @@
 zero.core.Item = CT.Class({
 	CLASSNAME: "zero.core.Item",
 	varieties: ["knocker", "smasher", "grabber", "flamer"],
+	_: {
+		wreck: function(thing, variety) {
+			if (!thing) return;
+			var zcc = zero.core.current, sploder = zcc.sploder;
+			this.log(variety, thing.name);
+			if (sploder)
+				sploder[variety](thing);
+			else
+				zcc.room.removeObject(thing);
+		}
+	},
+	smash: function() {
+		this._.wreck(zero.core.current.room.getBrittle(this), "shart");
+	},
+	melt: function() {
+		this._.wreck(zero.core.current.room.getFrozen(this), "melt");
+	},
+	burn: function() {
+		this._.wreck(zero.core.current.room.getFlammable(this), "burn");
+	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(zero.base.items[this.opts.name], opts, {
 			variety: "knocker"
