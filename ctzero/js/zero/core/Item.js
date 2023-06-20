d55ea6e4bdf2042b437454b92a4c1f9a63c9c0f7
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-19 19:17:09 -0700

    Item: varieties[] +grabber

--- ctzero/js/zero/core/Item.js
+++ ctzero/js/zero/core/Item.js
@@ -1,6 +1,6 @@
 zero.core.Item = CT.Class({
 	CLASSNAME: "zero.core.Item",
-	varieties: ["knocker", "smasher", "flamer"],
+	varieties: ["knocker", "smasher", "grabber", "flamer"],
 	init: function(opts) {
 		this.opts = opts = CT.merge(zero.base.items[this.opts.name], opts, {
 			variety: "knocker"
