bc5a8831bd274e86ab7aed96c34547840854d211
bubbleboy14 <mario.balibrera@gmail.com>
2023-07-19 02:21:42 -0700

    Fire.assembled(): switched back to calling glow.setBounds() w/ setTimeout()

--- ctzero/js/zero/core/Fire.js
+++ ctzero/js/zero/core/Fire.js
@@ -38,7 +38,7 @@ zero.core.Fire = CT.Class({
 	assembled: function() {
 		this._.built();
 		this.quenched && this.hide();
-		this.opts.glow && this.glow.setBounds();
+		this.opts.glow && setTimeout(this.glow.setBounds, 2000);
 		this.opts.regTick && zero.core.current.room.regTicker(this);
 	},
 	preassemble: function() {
