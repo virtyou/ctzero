c74f04a60e832661180ef7d936a7240179520dda
bubbleboy14 <mario.balibrera@gmail.com>
2023-07-24 14:53:32 -0700

    Leg.foot.offset()

--- ctzero/js/zero/core/Leg.js
+++ ctzero/js/zero/core/Leg.js
@@ -5,6 +5,9 @@ zero.core.Leg = CT.Class({
 		radii: { x: 10, y: 10, z: 10 }, // HACKY :'(
 		position: function(ignoredPos, world) {
 			return zero.core.util.getPos(this.toe, world);
+		},
+		offset: function() {
+			return this.body.position(null, true).y - this.foot.position(null, true).y;
 		}
 	}
 }, zero.core.Skeleton);
