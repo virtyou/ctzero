6dec7987963854415cc03a78b058473d5b80c6b7
bubbleboy14 <mario.balibrera@gmail.com>
2024-08-16 01:36:12 -0700

    Person.setVolume(): increased min to 0.4

--- ctzero/js/zero/core/Person.js
+++ ctzero/js/zero/core/Person.js
@@ -79,7 +79,7 @@ zero.core.Person = CT.Class({
 		this.body.group.__click && this.body.group.__click();
 	},
 	setVolume: function(v) {
-		this._.audio.volume = Math.max(0.1, v);
+		this._.audio.volume = Math.max(0.4, v);
 	},
 	afterSpeech: function(cb) {
 		this._.audio.onended = cb;
