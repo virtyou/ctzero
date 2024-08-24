549c269676d53db8146a0e38de08afe8d813c21b
bubbleboy14 <mario.balibrera@gmail.com>
2024-08-23 20:55:16 -0700

    Ramp: renamed onbound() to setSides(); setSides() now supports clear flag; onbound() calls setSides()

--- ctzero/js/zero/core/Ramp.js
+++ ctzero/js/zero/core/Ramp.js
@@ -20,7 +20,7 @@ zero.core.Ramp = CT.Class({
 			yper = zper * ydif;
 		return (rx > level) ? (ymax - yper) : (ymin + yper);
 	},
-	onbound: function() {
+	setSides: function(clear) {
 		var r = this.rotation(), attach = this.attach, doside = function(name, px) {
 			attach({
 				name: name,
@@ -33,7 +33,7 @@ zero.core.Ramp = CT.Class({
 				rotation: [rx, -r.y, -r.z]
 			});
 		}, rx = r.x, pd = rx % P4, backtilted = rx < P2, py = backtilted ? 25 : -25,
-			mat = this.material, side = this.opts.side, both = side == "both";
+			mat = this.material, side = this.opts.side, both = side == "both", oldside;
 		rx *= -1;
 		if (Math.abs(pd - P4) < 0.1)
 			pd -= P4;
@@ -41,10 +41,16 @@ zero.core.Ramp = CT.Class({
 			rx += pd;
 		else
 			rx -= pd;
+		if (clear && this.wall)
+			for (oldside in this.wall)
+				this[oldside].remove();
 		if (both || side == "right")
 			doside("right", 50);
 		if (both || side == "left")
 			doside("left", -50);
+	},
+	onbound: function() {
+		this.setSides();
 	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(opts, {
