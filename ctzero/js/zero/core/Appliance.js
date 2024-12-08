423fac6fbdd74245126125f5a3ceb26fc738cdcb
bubbleboy14 <mario.balibrera@gmail.com>
2024-12-07 16:29:49 -0800

    Appliance.varieties[]

--- ctzero/js/zero/core/Appliance.js
+++ ctzero/js/zero/core/Appliance.js
@@ -38,6 +38,7 @@ zero.core.Appliance = CT.Class({
 	}
 }, zero.core.Thing);
 
+zero.core.Appliance.varieties = ["panel", "bulb", "gate", "elevator", "computer"];
 zero.core.Appliance.templates = {}; // filled in by one
 
 zero.core.Appliance.tmpopts = function(app) {
