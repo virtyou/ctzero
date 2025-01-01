738f341f71c933eb9be2cae4c098dfe585c1a73e
bubbleboy14 <mario.balibrera@gmail.com>
2024-12-31 17:26:25 -0800

    Appliance.Computer, selectors.video(): vid (tl) pick()

--- ctzero/js/zero/core/Appliance.js
+++ ctzero/js/zero/core/Appliance.js
@@ -524,20 +524,24 @@ zero.core.Appliance.Computer.selectors = {
 		let fpref = "fzn:";
 		CT.modal.choice({
 			prompt: "what kind of video program?",
-			data: ["channel", "stream (down)", "stream (up)"],
+			data: ["channel", "video", "stream (down)", "stream (up)"],
 			cb: function(sel) {
-				if (sel == "channel") { // tl channel
-					return CT.modal.choice({
-						prompt: "what channel?", // TODO : avoid direct ctvu reference
-						data: core.config.ctvu.loaders.tlchans,
-						cb: chan => cb("tlchan:" + chan)
+				if (sel.startsWith("stream")) { // fzn stream
+					if (sel.includes("up"))
+						fpref += "up:";
+					return CT.modal.prompt({
+						prompt: "ok, what's the name of the stream?",
+						cb: name => cb(fpref + name)
 					});
-				} // fzn stream
-				if (sel.includes("up"))
-					fpref += "up:";
-				CT.modal.prompt({
-					prompt: "ok, what's the name of the stream?",
-					cb: name => cb(fpref + name)
+				} // tl video or channel
+				CT.modal.choice({
+					prompt: "what channel?", // TODO : avoid direct ctvu reference
+					data: core.config.ctvu.loaders.tlchans,
+					cb: function(chan) {
+						if (sel == "channel" || chan == "surf")
+							return cb("tlchan:" + chan);
+						CT.stream.util.tl.pick(chan, cb);
+					}
 				});
 			}
 		});
