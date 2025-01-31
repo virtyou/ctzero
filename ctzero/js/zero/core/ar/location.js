0138f0ada08ec7a23ec4d62b11d9e82a3e28a682
bubbleboy14 <mario.balibrera@gmail.com>
2025-01-31 15:15:53 -0800

    ar.location: gps and WebcamRenderer -related stuff

--- ctzero/js/zero/core/ar/location.js
+++ ctzero/js/zero/core/ar/location.js
@@ -4,47 +4,60 @@ zero.core.ar.location = {
 			latitude: 0,
 			longitude: 0
 		},
+		fakeGPS: true,
 		thing: function(t, i) {
 			var _ = zero.core.ar.location._, c = _.coords,
 				name = t.name || ("thing" + i);
 			_.things[name] = zero.core.util.thing(CT.merge(t, {
 				name: name,
-				adder: outerGroup => _.arloc.add(outerGroup,
+				adder: outerGroup => _.locar.add(outerGroup,
 					c.longitude + t.longitude, c.latitude + t.latitude)
 			}));
 		},
 		build: function() {
 			core.config.ctzero.camera.ar.things.forEach(zero.core.ar.location._.thing);
 		},
+		gps: function(dobuild) {
+			var zcar = zero.core.ar, _ = zcar.location._, cz = _.coords;
+			if (_.fakeGPS) {
+				_.locar.fakeGps(cz.longitude, cz.latitude);
+				dobuild();
+			} else {
+				_.locar.on("gpsupdate", function(pos) {
+					cz.latitude = pos.coords.latitude;
+					cz.longitude = pos.coords.longitude;
+					CT.log("GPS: " + cz.longitude + " lon; " + cz.latitude + " lat");
+					dobuild();
+				});
+				_.locar.on("gpserror", function(code) {
+					CT.log("GPS error: " + code);
+					dobuild();
+				});
+				_.locar.startGps();
+			}
+		},
 		init: function() {
-			var zcar = zero.core.ar, _ = zcar.location._, waiting = true, dobuild = function() {
+			var zcar = zero.core.ar, _ = zcar.location._, waiting = true;
+			_.things = {};
+			_.gps(function() {
 				if (waiting) {
 					waiting = false;
 					zcar.populate("things", _.build);
 				}
-			}, cz = _.coords;
-			_.things = {};
-			_.arloc.on("gpsupdate", function(pos) {
-				cz.latitude = pos.coords.latitude;
-				cz.longitude = pos.coords.longitude;
-				CT.log("GPS: " + cz.longitude + " lon; " + cz.latitude + " lat");
-				dobuild();
-			});
-			_.arloc.on("gpserror", function(code) {
-				CT.log("GPS error: " + code);
-				dobuild();
 			});
 		}
 	},
 	tick: function() {
 		var zcarlo = zc.ar.location, _ = zcarlo._;
 		_.orcon.update();
+		_.cam.update();
 	},
 	build: function() {
 		var zc = zero.core, cam = zc.camera, camcam = cam.get(),
 			zcarlo = zc.ar.location, _ = zcarlo._;
-		_.arloc = new THREEx.LocationBased(cam.scene, camcam);
+		_.locar = new THREEx.LocationBased(cam.scene, camcam);
 		_.orcon = new THREEx.DeviceOrientationControls(camcam);
+		_.cam = new THREEx.WebcamRenderer(cam.get("renderer"));
 		_.lights = core.config.ctzero.camera.ar.lights.map(zero.core.util.light);
 		_.init();
 	},
