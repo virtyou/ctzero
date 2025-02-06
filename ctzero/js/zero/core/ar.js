a3e6980697038de97f3c71c598791eb0e9a18f8d
bubbleboy14 <mario.balibrera@gmail.com>
2025-02-06 13:39:07 -0800

    ar: _{} with vidsies[], m2k(), latlng(), and gs2aug(); unit and viddy(); populate() supports justkey-defined (for location mode) things; start() supports #person=julian -style custom augmentations; item()

--- ctzero/js/zero/core/ar.js
+++ ctzero/js/zero/core/ar.js
@@ -1,5 +1,39 @@
 zero.core.ar = {
-	_: {},
+	_: {
+		vidsies: ["video", "program"],
+		m2k: function(mcfg) {
+			var m, kz = [];
+			for (m of Object.values(mcfg)) {
+				if (typeof m == "string")
+					kz.push(m);
+				else if (m.justkey)
+					kz.push(m.justkey);
+			}
+			return kz;
+		},
+		latlng: function() {
+			var zcar = zero.core.ar,
+				llpos = () => CT.data.choice([-zcar.unit, zcar.unit]);
+			return { latitude: llpos(), longitude: llpos() };
+		},
+		qs2aug: function(aqs) {
+			var zcar = zero.core.ar, aug = {
+				things: [],
+				lights: [{}],
+				name: "custom",
+				variety: "relocation"
+			}, latlng = zcar._.latlng, kind, val;
+			for (pair of aqs.split("&")) {
+				[kind, val] = pair.split("=");
+				aug.things.push(CT.merge(zcar.item(kind, val), latlng()));
+			}
+			return aug;
+		}
+	},
+	unit: 0.00004,
+	viddy: function(variety) {
+		return zero.core.ar._.vidsies.includes(variety);
+	},
 	tick: function() {
 		var zcar = zero.core.ar;
 		zcar[zcar.mode].tick();
@@ -59,9 +93,8 @@ zero.core.ar = {
 		});
 	},
 	populate: function(collection, builder) {
-		var zcar = zero.core.ar,
-			mcfg = core.config.ctzero.camera.ar[collection], m,
-			keys = Object.values(mcfg).filter(i => typeof i == "string");
+		var mcfg = core.config.ctzero.camera.ar[collection],
+			zcar = zero.core.ar, keys = zcar._.m2k(mcfg), m;
 		if (!keys.length)
 			return builder();
 		CT.db.multi(keys, function(things) {
@@ -69,6 +102,8 @@ zero.core.ar = {
 				for (m in mcfg)
 					if (mcfg[m] == thing.key)
 						mcfg[m] = thing;
+					else if (mcfg[m].justkey == thing.key)
+						mcfg[m] = CT.merge(mcfg[m], thing);
 			});
 			builder();
 			CT.cc.views(zcar.components());
@@ -84,10 +119,31 @@ zero.core.ar = {
 		});
 		return compz;
 	},
+	item: function(kind, val) {
+		var zcar = zero.core.ar, item = { kind: kind };
+		if (zcar.viddy(kind)) {
+			item.autoplay = "tap";
+			item.planeGeometry = [3, 3];
+			if (val) {
+				item.name = val.split("/").pop();
+				item.video = val;
+			}
+		} else if (kind == "swarm") {
+			item.name = val; // TODO: avoid direct one reference
+			item.thing = "Swarm";
+			item.frames = templates.one.vswarm[val];
+		} else if (kind == "person")
+			item.person = val.name || val;
+		else
+			item.justkey = val.key || val;
+		return item;
+	},
 	start: function(akey) {
-		var zcar = zero.core.ar;
-		if (akey)
-			return CT.db.one(akey, zcar.load);
+		var zcar = zero.core.ar, pair, key, val, aug;
+		if (akey) {
+			return (akey.includes("=")) ? zcar.load(zcar._.qs2aug(akey))
+				: CT.db.one(akey, zcar.load);
+		}
 		CT.modal.choice({
 			prompt: "anchors or location?",
 			data: ["location", "anchors"],
