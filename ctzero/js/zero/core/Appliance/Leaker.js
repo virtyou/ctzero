774bffd4c51a51abe0253cd41408ec58921146fe
bubbleboy14 <mario.balibrera@gmail.com>
2025-06-17 22:09:56 -0700

    Leaker drip stuff

--- ctzero/js/zero/core/Appliance/Leaker.js
+++ ctzero/js/zero/core/Appliance/Leaker.js
@@ -1,5 +1,12 @@
 zero.core.Appliance.Leaker = CT.Class({
 	CLASSNAME: "zero.core.Appliance.Leaker",
+	drip: function() {
+		const droz = this.opts.drip;
+		this.sound("drip");
+		this.puddle.drip(droz.size);
+		this.dripper = setTimeout(this.drip,
+			droz.rate + CT.data.random(droz.rate, true));
+	},
 	onready: function() {
 		const p = this.position(), r = zero.core.current.room;
 		this.puddle = r.addObject(CT.merge(this.opts.puddle, {
@@ -7,11 +14,23 @@ zero.core.Appliance.Leaker = CT.Class({
 			secondary: true,
 			position: [p.x, this.getBounds().min.y, p.z]
 		}));
+		this.puddle.onReady(this.drip);
+	},
+	onremove: function() {
+		this._audio && this._audio.pause();
+		clearTimeout(this.dripper);
+		delete this._audios;
+		delete this._audio;
+		this.unplug();
 	},
 	init: function(opts) {
 		this.opts = CT.merge(opts, {
 			puddle: {
 				radius: 100
+			},
+			drip: {
+				rate: 5000,
+				size: 0.05
 			}
 		}, this.opts);
 	}
